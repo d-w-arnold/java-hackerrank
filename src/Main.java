9528521973c4df6caa082b89b9d5b5eeeb6368e1
David W. Arnold <d.w.arnold.gm@gmail.com>
2021-01-23 23:56:07 +0000

    Added solution to extraLongFactorials()

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
-public class Main {
+import java.math.BigInteger;
 
-    public static void main(String[] args) {
-	// write your code here
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        extraLongFactorials(100);
+    }
+
+    /**
+     * @param n Input between 0-100
+     */
+    private static void extraLongFactorials(int n)
+    {
+        BigInteger bigInt = new BigInteger(String.valueOf(n));
+        for (int i = n - 1; i > 0; i--) {
+            bigInt = bigInt.multiply(new BigInteger(String.valueOf(i)));
+        }
+        System.out.println(bigInt.toString());
     }
 }
