4a91492cddb550acd06ba1064a1557c1a11c386d
David W. Arnold <d.w.arnold.gm@gmail.com>
2021-05-23 19:01:29 +0100

    Added hourGlassSum() solution

--- src/Main.java
+++ src/Main.java
@@ -2,26 +2,74 @@ import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 public class Main
 {
 
     public static void main(String[] args)
     {
+        System.out.println("\nextraLongFactorials");
+
         extraLongFactorials(50);
         extraLongFactorials(100);
 
+        System.out.println("\njumpingOnClouds");
+
         Integer[] array1 = {0, 0, 1, 0, 0, 1, 0};
-        System.out.println(jumpingOnClouds(new ArrayList<>(Arrays.asList(array1))) + "\n");
+        System.out.println(jumpingOnClouds(new ArrayList<>(Arrays.asList(array1))));
         Integer[] array2 = {0, 1, 0, 0, 0, 1, 0};
-        System.out.println(jumpingOnClouds(new ArrayList<>(Arrays.asList(array2))) + "\n");
+        System.out.println(jumpingOnClouds(new ArrayList<>(Arrays.asList(array2))));
         Integer[] array3 = {0, 0, 0, 0, 1, 0};
-        System.out.println(jumpingOnClouds(new ArrayList<>(Arrays.asList(array3))) + "\n");
+        System.out.println(jumpingOnClouds(new ArrayList<>(Arrays.asList(array3))));
+
+        System.out.println("\nrepeatedString");
 
         System.out.println(repeatedString("abcac", 10));
         System.out.println(repeatedString("aba", 10));
         System.out.println(repeatedString("a", 1000000000000L));
         System.out.println(repeatedString("kmretasscityylpdhuwjirnqimlkcgxubxmsxpypgzxtenweirknjtasxtvxemtwxuarabssvqdnktqadhyktagjxoanknhgilnm", 736778906400L));
+
+        System.out.println("\nhourglassSum");
+
+        Integer[][] twoDimArray1 = {
+                {-9, -9, -9, 1, 1, 1},
+                {0, -9, 0, 4, 3, 2},
+                {-9, -9, -9, 1, 2, 3},
+                {0, 0, 8, 6, 6, 0},
+                {0, 0, 0, -2, 0, 0},
+                {0, 0, 1, 2, 4, 0}
+        };
+        System.out.println(hourglassSum(covertTwoDimArrayToArrayList(twoDimArray1)));
+        Integer[][] twoDimArray2 = {
+                {1, 1, 1, 0, 0, 0},
+                {0, 1, 0, 0, 0, 0},
+                {1, 1, 1, 0, 0, 0},
+                {0, 0, 2, 4, 4, 0},
+                {0, 0, 0, 2, 0, 0},
+                {0, 0, 1, 2, 4, 0}
+        };
+        System.out.println(hourglassSum(covertTwoDimArrayToArrayList(twoDimArray2)));
+        Integer[][] twoDimArray3 = {
+                {-1, -1, 0, -9, -2, -2},
+                {-2, -1, -6, -8, -2, -5},
+                {-1, -1, -1, -2, -3, -4},
+                {-1, -9, -2, -4, -4, -5},
+                {-7, -3, -3, -2, -9, -9},
+                {-1, -3, -1, -2, -4, -5}
+        };
+        System.out.println(hourglassSum(covertTwoDimArrayToArrayList(twoDimArray3)));
+        Integer[][] twoDimArray4 = {
+                {0, -4, -6, 0, -7, -6},
+                {-1, -2, -6, -8, -3, -1},
+                {-8, -4, -2, -8, -8, -6},
+                {-3, -1, -2, -5, -7, -4},
+                {-3, -5, -3, -6, -6, -6},
+                {-3, -6, 0, -8, -6, -7}
+        };
+        System.out.println(hourglassSum(covertTwoDimArrayToArrayList(twoDimArray4)));
+
+        System.out.println("\n");
     }
 
     /**
@@ -35,7 +83,7 @@ public class Main
         for (int i = n - 1; i > 0; i--) {
             bigInt = bigInt.multiply(new BigInteger(String.valueOf(i)));
         }
-        System.out.println(bigInt + "\n");
+        System.out.println(bigInt);
     }
 
     /**
@@ -103,5 +151,85 @@ public class Main
             }
         }
         return count;
+    }
+
+    /**
+     * Hour Glass Sum problem: https://www.hackerrank.com/challenges/2d-array/problem
+     *
+     * @param arr An array of integers.
+     * @return The maximum hourglass sum.
+     */
+    public static int hourglassSum(List<List<Integer>> arr)
+    {
+        // Get all hourglass sums.
+        List<List<Integer>> sums = new ArrayList<>();
+        for (int i = 0; i < (arr.get(0).size() - 2); i++) {
+            List<Integer> rowOfSums = new ArrayList<>();
+            for (int j = 0; j < (arr.size() - 2); j++) {
+                rowOfSums.add(sumTwoDimArrayList(Objects.requireNonNull(getHourGlass(arr, i, j))));
+            }
+            sums.add(rowOfSums);
+        }
+        // Find the maximum hourglass sum.
+        int largestSum = 0;
+        boolean firstSum = true;
+        for (List<Integer> row : sums) {
+            for (int sum : row) {
+                if (firstSum) {
+                    largestSum = sum;
+                    firstSum = false;
+                } else if (sum > largestSum) {
+                    largestSum = sum;
+                }
+            }
+        }
+        return largestSum;
+    }
+
+    private static List<List<Integer>> covertTwoDimArrayToArrayList(Integer[][] twoDimArray)
+    {
+        List<List<Integer>> twoDimArrayList = new ArrayList<>();
+        for (Integer[] ints : twoDimArray) {
+            twoDimArrayList.add(Arrays.asList(ints));
+        }
+        return twoDimArrayList;
+    }
+
+    private static List<List<Integer>> getHourGlass(List<List<Integer>> twoDimArrayList, int x, int y)
+    {
+        if (x >= twoDimArrayList.get(0).size() || y >= twoDimArrayList.size()) {
+            return null;
+        }
+        List<List<Integer>> hourGlass = new ArrayList<>();
+        // Get first row: a,b,c
+        List<Integer> hourGlassFirstRow = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            hourGlassFirstRow.add(twoDimArrayList.get(x).get(y + i));
+        }
+        hourGlass.add(hourGlassFirstRow);
+        // Get next row: d
+        List<Integer> hourGlassSecondRow = new ArrayList<>();
+        hourGlassSecondRow.add(0);
+        hourGlassSecondRow.add(twoDimArrayList.get(x + 1).get(y + 1));
+        hourGlassSecondRow.add(0);
+        hourGlass.add(hourGlassSecondRow);
+        // Get last row: e,f,g
+        List<Integer> hourGlassThirdRow = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            hourGlassThirdRow.add(twoDimArrayList.get(x + 2).get(y + i));
+        }
+        hourGlass.add(hourGlassThirdRow);
+        return hourGlass;
+    }
+
+    private static int sumTwoDimArrayList(List<List<Integer>> twoDimArrayList)
+    {
+        int total = 0;
+        for (int i = 0; i < twoDimArrayList.get(0).size(); i++) {
+            for (int j = 0; j < twoDimArrayList.size(); j++) {
+                total += twoDimArrayList.get(i).get(j);
+            }
+        }
+        return total;
     }
 }
