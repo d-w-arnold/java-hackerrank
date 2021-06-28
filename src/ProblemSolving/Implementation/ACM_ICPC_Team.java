fadeb253abaa56dea66833edf15a7eaa78c1e616
David W. Arnold <d.w.arnold.gm@gmail.com>
2021-06-29 00:42:08 +0100

    Added first attempt at acmTeam() solution

--- src/ProblemSolving/Implementation/ACM_ICPC_Team.java
+++ src/ProblemSolving/Implementation/ACM_ICPC_Team.java
@@ -1,7 +1,6 @@
 package ProblemSolving.Implementation;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 /**
  * @author David W. Arnold
@@ -17,8 +16,33 @@ public class ACM_ICPC_Team
      */
     public static List<Integer> acmTeam(List<String> topic)
     {
+        List<Integer[]> pairs = new ArrayList<>();
+        Map<Integer[], List<Integer>> memSubjects = new LinkedHashMap<>();
+        for (int i = 1; i <= topic.size(); i++) {
+            for (int j = i + 1; j <= topic.size(); j++) {
+                Integer[] pair = new Integer[]{i, j};
+                pairs.add(pair);
+                memSubjects.put(pair, new ArrayList<>());
+            }
+        }
+        for (int i = 0; i < topic.get(0).length(); i++) {
+            for (Integer[] pair : pairs) {
+                if (topic.get(pair[0] - 1).charAt(i) == '1' || topic.get(pair[1] - 1).charAt(i) == '1') {
+                    memSubjects.get(pair).add(i + 1);
+                }
+            }
+        }
         int maxNumOfTopicsKnown = 0;
         int numOfTeams = 0;
+        for (Map.Entry<Integer[], List<Integer>> entry : memSubjects.entrySet()) {
+            int numOfTopics = entry.getValue().size();
+            if (numOfTopics > maxNumOfTopicsKnown) {
+                maxNumOfTopicsKnown = numOfTopics;
+                numOfTeams = 1;
+            } else if (numOfTopics == maxNumOfTopicsKnown) {
+                numOfTeams++;
+            }
+        }
         return Arrays.asList(maxNumOfTopicsKnown, numOfTeams);
     }
 }
