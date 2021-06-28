1d576b9cb39263ef839005aee00f218063878971
David W. Arnold <d.w.arnold.gm@gmail.com>
2021-06-28 17:33:14 +0100

    Added second attempt at queensAttack() solution

--- src/ProblemSolving/Implementation/Medium/QueensAttack2.java
+++ src/ProblemSolving/Implementation/Medium/QueensAttack2.java
@@ -1,6 +1,6 @@
 package ProblemSolving.Implementation.Medium;
 
-import java.util.*;
+import java.util.List;
 
 /**
  * @author David W. Arnold
@@ -20,79 +20,75 @@ public class QueensAttack2
      */
     public static int queensAttack(int n, int k, int r_q, int c_q, List<List<Integer>> obstacles)
     {
-        // Calculate starting number of squares the queen can attack without any obstacles.
-        int total = (n - 1) * 2;
-        total += Math.min(n - r_q, c_q - 1); // Top left diagonal
-        total += Math.min(n - r_q, n - c_q); // Top right diagonal
-        total += Math.min(r_q - 1, c_q - 1); // Bottom left diagonal
-        total += Math.min(r_q - 1, n - c_q); // Bottom right diagonal
-        System.out.println();
-        // Reduce total to account for obstacles.
-        if (k != 0) {
-            Set<List<Integer>> obstructedCells = new HashSet<>();
-            for (List<Integer> obstacle : obstacles) {
-                if (!obstructedCells.contains(new ArrayList<>(obstacle)) && isObstructing(r_q, c_q, obstacle.get(0), obstacle.get(1))) {
-                    updateObstructedCells(obstructedCells, n, r_q, c_q, obstacle.get(0), obstacle.get(1));
-                }
+        int squaresQueenCanAttack = 0;
+        int r_RightObs = -1;
+        int c_RightObs = -1;
+        int r_BelowRightObs = -1;
+        int c_BelowRightObs = -1;
+        int r_BelowObs = -1;
+        int c_BelowObs = -1;
+        int r_BelowLeftObs = -1;
+        int c_BelowLeftObs = -1;
+        int r_LeftObs = -1;
+        int c_LeftObs = -1;
+        int r_AboveLeftObs = -1;
+        int c_AboveLeftObs = -1;
+        int r_AboveObs = -1;
+        int c_AboveObs = -1;
+        int r_AboveRightObs = -1;
+        int c_AboveRightObs = -1;
+        for (int i = 0; i < k; i++) { // Find the closest obstacles in each direction
+            int r_obs = obstacles.get(i).get(0);
+            int c_obs = obstacles.get(i).get(1);
+            // Above
+            if ((r_obs < r_AboveObs || r_AboveObs == -1) && r_obs > r_q && c_obs == c_q) {
+                r_AboveObs = r_obs;
+                c_AboveObs = c_obs;
             }
-            return total - obstructedCells.size();
-        } else {
-            return total;
+            // Below
+            if ((r_obs > r_BelowObs || r_BelowObs == -1) && r_obs < r_q && c_obs == c_q) {
+                r_BelowObs = r_obs;
+                c_BelowObs = c_obs;
+            }
+            // Left
+            if ((c_obs > c_LeftObs || r_LeftObs == -1) && c_obs < c_q && r_obs == r_q) {
+                r_LeftObs = r_obs;
+                c_LeftObs = c_obs;
+            }
+            // Right
+            if ((c_obs < c_RightObs || r_RightObs == -1) && c_obs > c_q && r_obs == r_q) {
+                r_RightObs = r_obs;
+                c_RightObs = c_obs;
+            }
+            // Above Left
+            if (c_q - c_obs == r_obs - r_q && c_obs < c_q && r_obs > r_q && ((r_obs < r_AboveLeftObs && c_obs > c_AboveLeftObs) || r_AboveLeftObs == -1)) {
+                r_AboveLeftObs = r_obs;
+                c_AboveLeftObs = c_obs;
+            }
+            // Above Right
+            if (r_obs - r_q == c_obs - c_q && c_obs > c_q && r_obs > r_q && ((r_obs < r_AboveRightObs && c_obs < c_AboveRightObs) || r_AboveRightObs == -1)) {
+                r_AboveRightObs = r_obs;
+                c_AboveRightObs = c_obs;
+            }
+            // Below Left
+            if (r_q - r_obs == c_q - c_obs && c_obs < c_q && r_obs < r_q && ((r_obs > r_BelowLeftObs && c_obs > c_BelowLeftObs) || r_BelowLeftObs == -1)) {
+                r_BelowLeftObs = r_obs;
+                c_BelowLeftObs = c_obs;
+            }
+            // Below Right
+            if (r_q - r_obs == c_obs - c_q && c_obs > c_q && r_obs < r_q && ((r_obs > r_BelowRightObs && c_obs < c_BelowRightObs) || r_BelowRightObs == -1)) {
+                r_BelowRightObs = r_obs;
+                c_BelowRightObs = c_obs;
+            }
         }
-    }
-
-    private static void updateObstructedCells(Set<List<Integer>> obstructedCells, int n, int r_q, int c_q, int r_obs, int c_obs)
-    {
-        int r_diff = r_obs - r_q;
-        int c_diff = c_obs - c_q;
-        if (r_diff == 0) { // Same row
-            if (c_diff < 0) { // Left of queen
-                c_diff = -1;
-            } else { // Right of queen
-                c_diff = 1;
-            }
-            updateObstructedCellsHelper(obstructedCells, n, r_obs, c_obs, r_diff, c_diff);
-        } else if (c_diff == 0) { // Same col
-            if (r_diff > 0) { // Above queen
-                r_diff = 1;
-            } else { // Below queen
-                r_diff = -1;
-            }
-            updateObstructedCellsHelper(obstructedCells, n, r_obs, c_obs, r_diff, c_diff);
-        } else { // Must be on diag
-            if ((r_diff > 0 && c_diff < 0) || (r_diff < 0 && c_diff > 0)) { // Bottom left to top right diag
-                if (r_diff > 0) { // Above left of queen
-                    r_diff = 1;
-                    c_diff = -1;
-                } else { // Below right of queen
-                    r_diff = -1;
-                    c_diff = 1;
-                }
-                updateObstructedCellsHelper(obstructedCells, n, r_obs, c_obs, r_diff, c_diff);
-            } else { // Top left to bottom right diag
-                if (r_diff > 0) { // Above right of queen
-                    r_diff = 1;
-                    c_diff = 1;
-                } else { // Below left of queen
-                    r_diff = -1;
-                    c_diff = -1;
-                }
-                updateObstructedCellsHelper(obstructedCells, n, r_obs, c_obs, r_diff, c_diff);
-            }
-        }
-    }
-
-    private static void updateObstructedCellsHelper(Set<List<Integer>> obstructedCells, int n, int r_obs, int c_obs, int r_diff, int c_diff)
-    {
-        while (0 < r_obs && r_obs <= n && 0 < c_obs && c_obs <= n) {
-            obstructedCells.add(new ArrayList<>(Arrays.asList(r_obs, c_obs)));
-            r_obs += r_diff;
-            c_obs += c_diff;
-        }
-    }
-
-    private static boolean isObstructing(int r_q, int c_q, int r_obs, int c_obs)
-    {
-        return r_q == r_obs || c_q == c_obs || Math.abs(r_q - r_obs) == Math.abs(c_q - c_obs);
+        squaresQueenCanAttack += (r_AboveObs != -1) ? (r_AboveObs - r_q - 1) : n - r_q; // Squares the queen can attack above
+        squaresQueenCanAttack += (r_BelowObs != -1) ? (r_q - r_BelowObs - 1) : r_q - 1; // Squares the queen can attack below
+        squaresQueenCanAttack += (c_LeftObs != -1) ? (c_q - c_LeftObs - 1) : c_q - 1; // Squares the queen can attack to the left
+        squaresQueenCanAttack += (c_RightObs != -1) ? (c_RightObs - c_q - 1) : n - c_q; // Squares the queen can attack to the right
+        squaresQueenCanAttack += (c_AboveLeftObs != -1) ? (c_q - c_AboveLeftObs - 1) : Math.min(c_q - 1, n - r_q); // Squares the queen can attack above and left
+        squaresQueenCanAttack += (r_AboveRightObs != -1) ? (c_AboveRightObs - c_q - 1) : Math.min(n - c_q, n - r_q); // Squares the queen can attack above and right
+        squaresQueenCanAttack += (r_BelowLeftObs != -1) ? (c_q - c_BelowLeftObs - 1) : Math.min(c_q - 1, r_q - 1); // Squares the queen can attack below and left
+        squaresQueenCanAttack += (c_BelowRightObs != -1) ? (c_BelowRightObs - c_q - 1) : Math.min(n - c_q, r_q - 1); // Squares the queen can attack below and right
+        return squaresQueenCanAttack;
     }
 }
