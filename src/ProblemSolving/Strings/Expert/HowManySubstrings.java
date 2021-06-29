bc324b72b338f9afc3fe394c5b8adde8836014cd
David W. Arnold <d.w.arnold.gm@gmail.com>
2021-06-29 11:22:45 +0100

    Added first attempt at countSubstrings() solution

--- src/ProblemSolving/Strings/Expert/HowManySubstrings.java
+++ src/ProblemSolving/Strings/Expert/HowManySubstrings.java
@@ -1,7 +1,6 @@
 package ProblemSolving.Strings.Expert;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * @author David W. Arnold
@@ -11,6 +10,7 @@ public class HowManySubstrings
 {
     /**
      * How Many Substrings? problem: https://www.hackerrank.com/challenges/how-many-substrings/problem
+     * TODO: Improve on 30/100 score, look into using Ukkonen's algorithm.
      *
      * @param s       A string of 'n' characters.
      * @param queries A list of queries in the form of two integer indices: left and right.
@@ -18,6 +18,39 @@ public class HowManySubstrings
      */
     public static List<Integer> countSubstrings(String s, List<List<Integer>> queries)
     {
-        return new ArrayList<>();
+        List<Integer> ans = new ArrayList<>();
+        Map<String, Integer> map = new HashMap<>();
+        for (List<Integer> query : queries) {
+            int left = query.get(0);
+            int right = query.get(1);
+            String tmpPair = getStrPair(left, right);
+            if (map.containsKey(tmpPair)) { // Already seen pair found
+                ans.add(map.get(tmpPair));
+            } else if (left == right) { // Not already seen pair found, and only 1 substring possible
+                ans.add(1);
+                map.put(tmpPair, 1);
+            } else { // Not already seen pair found, and more than 1 substring possible
+                int numOfSubstring = getNumOfSubstrings(s.substring(left, right + 1));
+                ans.add(numOfSubstring);
+                map.put(tmpPair, numOfSubstring);
+            }
+        }
+        return ans;
+    }
+
+    private static String getStrPair(int left, int right)
+    {
+        return left + "-" + right;
+    }
+
+    private static int getNumOfSubstrings(String str)
+    {
+        Set<String> substrings = new HashSet<>();
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = i; j < str.length(); j++) {
+                substrings.add(str.substring(i, j + 1));
+            }
+        }
+        return substrings.size();
     }
 }
