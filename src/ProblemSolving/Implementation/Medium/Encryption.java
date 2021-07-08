e82a396deb6db74c8f94c87b7f6683b483d3b3e9
David W. Arnold <d.w.arnold.gm@gmail.com>
2021-07-08 17:53:45 +0100

    Added encryption() solution

--- src/ProblemSolving/Implementation/Medium/Encryption.java
+++ src/ProblemSolving/Implementation/Medium/Encryption.java
@@ -1,5 +1,7 @@
 package ProblemSolving.Implementation.Medium;
 
+import java.util.LinkedHashMap;
+
 /**
  * @author David W. Arnold
  * @version 08/07/2021
@@ -14,6 +16,35 @@ public class Encryption
      */
     public static String encryption(String s)
     {
-        return "NULL";
+        int row, col;
+        int sr = (int) Math.sqrt(s.length());
+        if (isPerfectSquare(s.length())) {
+            row = sr;
+            col = sr;
+        } else {
+            row = sr;
+            col = row + 1;
+        }
+        LinkedHashMap<Integer, String> cols = new LinkedHashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char tmp = s.charAt(i);
+            int colIndex = i % col;
+            if (cols.containsKey(colIndex)) {
+                cols.put(colIndex, cols.get(colIndex) + tmp);
+            } else {
+                cols.put(colIndex, String.valueOf(tmp));
+            }
+        }
+        return String.join(" ", cols.values());
+    }
+
+    private static boolean isPerfectSquare(int s)
+    {
+        if (s >= 0) {
+            int sr = (int) Math.sqrt(s);
+            System.out.println();
+            return ((sr * sr) == s);
+        }
+        return false;
     }
 }
