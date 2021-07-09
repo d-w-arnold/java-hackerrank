eff050fa183de89b65a8abba9b58eb40f127c763
David W. Arnold <d.w.arnold.gm@gmail.com>
2021-07-09 15:44:07 +0100

    Added sherlockAndAnagrams() solution

--- src/InterviewPreparationKit/DictionariesAndHashmaps/Medium/SherlockAndAnagrams.java
+++ src/InterviewPreparationKit/DictionariesAndHashmaps/Medium/SherlockAndAnagrams.java
@@ -1,5 +1,8 @@
 package InterviewPreparationKit.DictionariesAndHashmaps.Medium;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author David W. Arnold
  * @version 09/07/2021
@@ -14,6 +17,30 @@ public class SherlockAndAnagrams
      */
     public static int sherlockAndAnagrams(String s)
     {
-        return -2;
+        Map<Map<Character, Integer>, Integer> buckets = new HashMap<>();
+        Map<Character, Integer> freqMap;
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = 1; j < s.length() - i + 1; j++) {
+                freqMap = new HashMap<>();
+                for (char c : s.substring(i, i + j).toCharArray()) {
+                    if (freqMap.containsKey(c)) {
+                        freqMap.put(c, freqMap.get(c) + 1);
+                    } else {
+                        freqMap.put(c, 1);
+                    }
+                }
+                final Map<Character, Integer> key = new HashMap<>(freqMap);
+                if (buckets.containsKey(key)) {
+                    buckets.put(key, buckets.get(key) + 1);
+                } else {
+                    buckets.put(key, 1);
+                }
+            }
+        }
+        for (int value : buckets.values()) {
+            count += value * (value - 1) / 2;
+        }
+        return count;
     }
 }
