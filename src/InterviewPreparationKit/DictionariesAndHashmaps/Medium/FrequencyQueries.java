55d09e93f8304ffd41e8f6c0a0710f192e0de645
David W. Arnold <d.w.arnold.gm@gmail.com>
2021-07-21 15:45:18 +0100

    Added third attempt freqQuery() solution

--- src/InterviewPreparationKit/DictionariesAndHashmaps/Medium/FrequencyQueries.java
+++ src/InterviewPreparationKit/DictionariesAndHashmaps/Medium/FrequencyQueries.java
@@ -1,9 +1,6 @@
 package InterviewPreparationKit.DictionariesAndHashmaps.Medium;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author David W. Arnold
@@ -19,50 +16,59 @@ public class FrequencyQueries
      */
     public static List<Integer> freqQuery(List<List<Integer>> queries)
     {
-        Map<Integer, Integer> freqMap = new HashMap<>();
+        Map<Integer, Integer> numFreq = new HashMap<>();
+        Map<Integer, Set<Integer>> freqNum = new HashMap<>();
         List<Integer> array = new ArrayList<>();
         List<Integer> output = new ArrayList<>();
         for (List<Integer> query : queries) {
             switch (query.get(0)) {
                 case 1:
-                    opOne(query.get(1), freqMap, array);
+                    opOne(query.get(1), numFreq, freqNum, array);
                     break;
                 case 2:
-                    opTwo(query.get(1), freqMap, array);
+                    opTwo(query.get(1), numFreq, freqNum, array);
                     break;
                 case 3:
-                    opThree(query.get(1), freqMap, output);
+                    opThree(query.get(1), freqNum, output);
                     break;
             }
         }
         return output;
     }
 
-    private static void opOne(int n, Map<Integer, Integer> freqMap, List<Integer> array)
+    private static void opOne(int n, Map<Integer, Integer> numFreq, Map<Integer, Set<Integer>> freqNum, List<Integer> array)
     {
         array.add(n);
-        freqMap.put(n, freqMap.containsKey(n) ? freqMap.get(n) + 1 : 1);
+        if (numFreq.containsKey(n)) {
+            int oldCount = numFreq.get(n);
+            int newCount = oldCount + 1;
+            numFreq.put(n, newCount);
+            freqNum.get(oldCount).remove(n);
+            if (!freqNum.containsKey(newCount)) freqNum.put(newCount, new HashSet<>());
+            freqNum.get(newCount).add(n);
+        } else {
+            numFreq.put(n, 1);
+            freqNum.put(1, new HashSet<>(Collections.singletonList(n)));
+        }
     }
 
-    private static void opTwo(int n, Map<Integer, Integer> freqMap, List<Integer> array)
+    private static void opTwo(int n, Map<Integer, Integer> numFreq, Map<Integer, Set<Integer>> freqNum, List<Integer> array)
     {
         for (int i = 0; i < array.size(); i++) {
             if (array.get(i) == n) {
                 array.remove(i);
-                freqMap.put(n, freqMap.get(n) - 1);
+                int oldCount = numFreq.get(n);
+                int newCount = oldCount - 1;
+                numFreq.put(n, newCount);
+                freqNum.get(oldCount).remove(n);
+                if (newCount > 0) freqNum.get(newCount).add(n);
                 return;
             }
         }
     }
 
-    private static void opThree(int n, Map<Integer, Integer> freqMap, List<Integer> output)
+    private static void opThree(int n, Map<Integer, Set<Integer>> freqNum, List<Integer> output)
     {
-        for (Map.Entry<Integer, Integer> entry : freqMap.entrySet()) {
-            if (entry.getValue() == n) {
-                output.add(1);
-                return;
-            }
-        }
-        output.add(0);
+        output.add(freqNum.containsKey(n) && freqNum.get(n).size() > 0 ? 1 : 0);
     }
 }
