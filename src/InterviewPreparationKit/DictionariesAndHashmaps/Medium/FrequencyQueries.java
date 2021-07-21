5072c2e709fc58ed74fae4f39686b29f6a460b7d
David W. Arnold <d.w.arnold.gm@gmail.com>
2021-07-21 14:54:30 +0100

    Added first attempt freqQuery() solution

--- src/InterviewPreparationKit/DictionariesAndHashmaps/Medium/FrequencyQueries.java
+++ src/InterviewPreparationKit/DictionariesAndHashmaps/Medium/FrequencyQueries.java
@@ -1,7 +1,9 @@
 package InterviewPreparationKit.DictionariesAndHashmaps.Medium;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author David W. Arnold
@@ -17,6 +19,56 @@ public class FrequencyQueries
      */
     public static List<Integer> freqQuery(List<List<Integer>> queries)
     {
-        return new ArrayList<>();
+        List<Integer> array = new ArrayList<>();
+        List<Integer> output = new ArrayList<>();
+        for (List<Integer> query : queries) {
+            switch (query.get(0)) {
+                case 1:
+                    array.add(query.get(1));
+                    break;
+                case 2:
+                    opTwo(query.get(1), array);
+                    break;
+                case 3:
+                    opThree(query.get(1), array, output);
+                    break;
+            }
+        }
+        return output;
+    }
+
+    private static void opTwo(int n, List<Integer> array)
+    {
+        for (int i = 0; i < array.size(); i++) {
+            if (array.get(i) == n) {
+                array.remove(i);
+                return;
+            }
+        }
+    }
+
+    private static void opThree(int n, List<Integer> array, List<Integer> output)
+    {
+        Map<Integer, Integer> freqMap = getFreqMap(array);
+        for (Map.Entry<Integer, Integer> entry : freqMap.entrySet()) {
+            if (entry.getValue() == n) {
+                output.add(1);
+                return;
+            }
+        }
+        output.add(0);
+    }
+
+    private static Map<Integer, Integer> getFreqMap(List<Integer> list)
+    {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i : list) {
+            if (map.containsKey(i)) {
+                map.put(i, map.get(i) + 1);
+            } else {
+                map.put(i, 1);
+            }
+        }
+        return map;
     }
 }
