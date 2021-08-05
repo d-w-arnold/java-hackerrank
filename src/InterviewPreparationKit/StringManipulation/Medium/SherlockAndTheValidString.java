3139a16c9eec46cb318be126a4b1dd91c700a6f9
David W. Arnold <d.w.arnold.gm@gmail.com>
2021-08-05 15:28:42 +0100

    Added first attempt isValid() solution

--- src/InterviewPreparationKit/StringManipulation/Medium/SherlockAndTheValidString.java
+++ src/InterviewPreparationKit/StringManipulation/Medium/SherlockAndTheValidString.java
@@ -1,5 +1,9 @@
 package InterviewPreparationKit.StringManipulation.Medium;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * @author David W. Arnold
  * @version 05/08/2021
@@ -14,6 +18,31 @@ public class SherlockAndTheValidString
      */
     public static String isValid(String s)
     {
-        return "NULL";
+        Map<Character, Integer> freqMap = new HashMap<>();
+        Map<Integer, Integer> freqMapOcc = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            boolean exists = freqMap.containsKey(c);
+            freqMap.put(c, exists ? freqMap.get(c) + 1 : 1);
+            if (exists) {
+                int newVal = freqMap.get(c);
+                int oldVal = newVal - 1;
+                freqMapOcc.put(newVal, freqMapOcc.containsKey(newVal) ? freqMapOcc.get(newVal) + 1 : 1);
+                if (freqMapOcc.get(oldVal) > 1) {
+                    freqMapOcc.put(oldVal, freqMapOcc.get(oldVal) - 1);
+                } else {
+                    freqMapOcc.remove(oldVal);
+                }
+            } else {
+                freqMapOcc.put(1, freqMapOcc.containsKey(1) ? freqMapOcc.get(1) + 1 : 1);
+            }
+        }
+        if (freqMapOcc.size() == 1) return "YES";
+        if (freqMapOcc.size() == 2) {
+            Set<Integer> keySet = freqMapOcc.keySet();
+            for (int key : keySet) {
+                if (freqMapOcc.get(key) == 1) return keySet.contains(key - 1) ? "YES" : "NO";
+            }
+        }
+        return "NO";
     }
 }
