3adb6dd4ba03c0a837344c5ee07f375eb08ec055
David W. Arnold <d.w.arnold.gm@gmail.com>
2021-08-10 14:48:25 +0100

    Added luckBalance() solution

--- src/InterviewPreparationKit/GreedyAlgorithms/LuckBalance.java
+++ src/InterviewPreparationKit/GreedyAlgorithms/LuckBalance.java
@@ -1,5 +1,6 @@
 package InterviewPreparationKit.GreedyAlgorithms;
 
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -17,6 +18,19 @@ public class LuckBalance
      */
     public static int luckBalance(int k, List<List<Integer>> contests)
     {
-        return -2;
+        int totalLuck = 0;
+        contests.sort(((Comparator<List<Integer>>) (o1, o2) -> o1.get(1).equals(o2.get(1)) ? o1.get(0).compareTo(o2.get(0)) : o1.get(1) < o2.get(1) ? -1 : 1).reversed());
+        for (List<Integer> entry : contests) {
+            int luck = entry.get(0);
+            if (entry.get(1) == 0) {
+                totalLuck += luck;
+            } else if (k > 0) {
+                totalLuck += luck;
+                k -= 1;
+            } else {
+                totalLuck -= luck;
+            }
+        }
+        return totalLuck;
     }
 }
