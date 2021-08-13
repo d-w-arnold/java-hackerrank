72701e2e431ad82413ea49c5cd15a1a31af0eed5
David W. Arnold <d.w.arnold.gm@gmail.com>
2021-08-13 18:08:26 +0100

    Added first attempt swapNodes() solution - tests 2 and 3 failing

--- src/InterviewPreparationKit/Search/Medium/SwapNodesAlgo.java
+++ src/InterviewPreparationKit/Search/Medium/SwapNodesAlgo.java
@@ -1,7 +1,9 @@
 package InterviewPreparationKit.Search.Medium;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author David W. Arnold
@@ -18,6 +20,130 @@ public class SwapNodesAlgo
      */
     public static List<List<Integer>> swapNodes(List<List<Integer>> indexes, List<Integer> queries)
     {
-        return Arrays.asList(Arrays.asList(-2));
+        List<List<Integer>> ans = new ArrayList<>();
+        BinaryTree tree = new BinaryTree(indexes);
+        for (int k : queries) {
+            tree.swap(k);
+            ans.add(getListFromInOrderString(tree.inOrderString));
+        }
+        return ans;
+    }
+
+    private static List<Integer> getListFromInOrderString(String str)
+    {
+        return Arrays.stream(str.split(",")).map(Integer::parseInt).collect(Collectors.toList());
+    }
+
+    private static class BinaryTree
+    {
+        Node root;
+        String inOrderString;
+
+        public BinaryTree(List<List<Integer>> indexes)
+        {
+            this.root = insert(getArrayFromIndexes(indexes), null, 0);
+            genInOrderString();
+        }
+
+        public Node insert(int[] arr, Node root, int i)
+        {
+            if (i < arr.length && arr[i] != -1) {
+                root = new Node(arr[i]);
+                root.left = insert(arr, root.left, 2 * i + 1);
+                root.right = insert(arr, root.right, 2 * i + 2);
+            }
+            return root;
+        }
+
+        public void swap(int k)
+        {
+            swapHelper(this.root, 1, getSwapDepths(k, getTreeDepth()));
+            genInOrderString();
+        }
+
+        public void genInOrderString()
+        {
+            this.inOrderString = "";
+            genInOrderStringHelper(this.root);
+        }
+
+        private int[] getArrayFromIndexes(List<List<Integer>> indexes)
+        {
+            int[] newIndexes = new int[(indexes.size() * indexes.get(0).size()) + 1];
+            newIndexes[0] = 1;
+            int index = 1;
+            for (List<Integer> list : indexes) {
+                newIndexes[index++] = list.get(0);
+                newIndexes[index++] = list.get(1);
+            }
+            return newIndexes;
+        }
+
+        private List<Integer> getSwapDepths(int k, int treeDepth)
+        {
+            List<Integer> list = new ArrayList<>();
+            if (k > 1) {
+                int i = k;
+                while (i <= treeDepth) {
+                    list.add(i);
+                    i *= k;
+                }
+            } else if (k == 1) {
+                for (int i = 1; i <= treeDepth; i++) {
+                    list.add(i);
+                }
+            }
+            return list;
+        }
+
+        private int getTreeDepth()
+        {
+            return getTreeDepthHelper(this.root);
+        }
+
+        private void swapHelper(Node node, int depth, List<Integer> swapDepths)
+        {
+            if (swapDepths.size() > 0) {
+                if (depth == swapDepths.get(0)) {
+                    swapDepths.remove(0);
+                    Node tmp = node.left;
+                    node.left = node.right;
+                    node.right = tmp;
+                }
+                if (node.left != null) swapHelper(node.left, depth + 1, swapDepths);
+                if (node.right != null) swapHelper(node.right, depth + 1, swapDepths);
+            }
+        }
+
+        private void genInOrderStringHelper(Node root)
+        {
+            if (root != null) {
+                genInOrderStringHelper(root.left);
+                this.inOrderString += root.data + ",";
+                genInOrderStringHelper(root.right);
+            }
+        }
+
+        private int getTreeDepthHelper(Node node)
+        {
+            if (node == null) return 0;
+            int lDepth = getTreeDepthHelper(node.left);
+            int rDepth = getTreeDepthHelper(node.right);
+            return lDepth > rDepth ? (lDepth + 1) : (rDepth + 1);
+        }
+
+        private static class Node
+        {
+            int data;
+            Node left;
+            Node right;
+
+            Node(int data)
+            {
+                this.data = data;
+                this.left = null;
+                this.right = null;
+            }
+        }
     }
 }
