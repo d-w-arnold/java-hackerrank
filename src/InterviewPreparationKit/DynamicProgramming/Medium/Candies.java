fc59a515fea64401c7319358d8c0d2cf6445c567
David W. Arnold <d.w.arnold.gm@gmail.com>
2021-08-17 17:41:08 +0100

    Added first attempt candies() solution

--- src/InterviewPreparationKit/DynamicProgramming/Medium/Candies.java
+++ src/InterviewPreparationKit/DynamicProgramming/Medium/Candies.java
@@ -17,6 +17,18 @@ public class Candies
      */
     public static long candies(int n, List<Integer> arr)
     {
-        return -2;
+        int[] candies = new int[n];
+        candies[0] = 1;
+        for (int i = 1; i < n; i++) {
+            candies[i] = arr.get(i) > arr.get(i - 1) ? candies[i - 1] + 1 : 1;
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            if (arr.get(i) > arr.get(i + 1) && candies[i] <= candies[i + 1]) candies[i] = candies[i + 1] + 1;
+        }
+        long total = 0;
+        for (int c : candies) {
+            total += c;
+        }
+        return total;
     }
 }
