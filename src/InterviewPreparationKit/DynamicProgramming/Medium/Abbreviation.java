23dd9394f1dbe3a34b18b149573d6e484fe4fd28
David W. Arnold <d.w.arnold.gm@gmail.com>
2021-08-17 16:27:23 +0100

    Started abbreviation() solution

---
+++ src/InterviewPreparationKit/DynamicProgramming/Medium/Abbreviation.java
@@ -0,0 +1,20 @@
+package InterviewPreparationKit.DynamicProgramming.Medium;
+
+/**
+ * @author David W. Arnold
+ * @version 17/08/2021
+ */
+public class Abbreviation
+{
+    /**
+     * Abbreviation problem: https://www.hackerrank.com/challenges/abbr/problem
+     *
+     * @param a The string to modify.
+     * @param b The string to match.
+     * @return Either YES or NO.
+     */
+    public static String abbreviation(String a, String b)
+    {
+        return "NULL";
+    }
+}
---
+++ tests/InterviewPreparationKit/DynamicProgramming/Medium/AbbreviationTest.java
@@ -0,0 +1,43 @@
+package InterviewPreparationKit.DynamicProgramming.Medium;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author David W. Arnold
+ * @version 17/08/2021
+ */
+class AbbreviationTest
+{
+
+    @BeforeEach
+    void setUp()
+    {
+    }
+
+    @AfterEach
+    void tearDown()
+    {
+    }
+
+    @Test
+    void abbreviation_1()
+    {
+        assertEquals("YES", Abbreviation.abbreviation("AbcDE", "ABDE"));
+    }
+
+    @Test
+    void abbreviation_2()
+    {
+        assertEquals("NO", Abbreviation.abbreviation("AbcDE", "AFDE"));
+    }
+
+    @Test
+    void abbreviation_3()
+    {
+        assertEquals("YES", Abbreviation.abbreviation("daBcd", "ABC"));
+    }
+}
