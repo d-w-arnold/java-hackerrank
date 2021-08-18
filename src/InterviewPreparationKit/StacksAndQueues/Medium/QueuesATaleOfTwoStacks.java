920577bb35bbe5eb70c047dd77af690f7e576a36
David W. Arnold <d.w.arnold.gm@gmail.com>
2021-08-18 17:15:26 +0100

    Added second attempt QueuesATaleOfTwoStacks class solution

--- src/InterviewPreparationKit/StacksAndQueues/Medium/QueuesATaleOfTwoStacks.java
+++ src/InterviewPreparationKit/StacksAndQueues/Medium/QueuesATaleOfTwoStacks.java
@@ -1,6 +1,6 @@
 package InterviewPreparationKit.StacksAndQueues.Medium;
 
-import java.util.ArrayList;
+import java.util.Stack;
 
 /**
  * @author David W. Arnold
@@ -8,18 +8,16 @@ import java.util.ArrayList;
  */
 public class QueuesATaleOfTwoStacks<T>
 {
-    private final ArrayList<T> LEFT_STACK;
-    private final ArrayList<T> RIGHT_STACK;
-    private int lastMethodType;
+    private final Stack<T> NORMAL;
+    private final Stack<T> REVERSED;
 
     /**
      * Queues: A Tale of Two Stacks problem: https://www.hackerrank.com/challenges/ctci-queue-using-two-stacks/problem
      */
     public QueuesATaleOfTwoStacks()
     {
-        this.LEFT_STACK = new ArrayList<>();
-        this.RIGHT_STACK = new ArrayList<>();
-        this.lastMethodType = -1;
+        NORMAL = new Stack<>();
+        REVERSED = new Stack<>();
     }
 
     /**
@@ -29,9 +27,14 @@ public class QueuesATaleOfTwoStacks<T>
      */
     public void enqueue(T item)
     {
-        if (lastMethodType == 2 || lastMethodType == 3) unShuffleStacks();
-        LEFT_STACK.add(item);
-        lastMethodType = 1;
+        NORMAL.push(item);
+    }
+
+    private void pour()
+    {
+        while (!NORMAL.isEmpty()) {
+            REVERSED.push(NORMAL.pop());
+        }
     }
 
     /**
@@ -39,9 +42,8 @@ public class QueuesATaleOfTwoStacks<T>
      */
     public void dequeue()
     {
-        if (lastMethodType == 1) shuffleStacks();
-        RIGHT_STACK.remove(RIGHT_STACK.size() - 1);
-        lastMethodType = 2;
+        if (REVERSED.isEmpty()) pour();
+        REVERSED.pop();
     }
 
     /**
@@ -49,32 +51,7 @@ public class QueuesATaleOfTwoStacks<T>
      */
     public T peek()
     {
-        if (lastMethodType == 1) shuffleStacks();
-        lastMethodType = 3;
-        return RIGHT_STACK.get(RIGHT_STACK.size() - 1);
-    }
-
-    /**
-     * Reverses order: LEFT_STACK -> RIGHT_STACK.
-     * Sets up for either a dequeue() or peek() method.
-     */
-    private void shuffleStacks()
-    {
-        for (int i = LEFT_STACK.size() - 1; i >= 0; i--) {
-            T item = LEFT_STACK.remove(i);
-            RIGHT_STACK.add(item);
-        }
-    }
-
-    /**
-     * Reverses order: LEFT_STACK <- RIGHT_STACK.
-     * Sets up for an enqueue() method.
-     */
-    private void unShuffleStacks()
-    {
-        for (int i = RIGHT_STACK.size() - 1; i >= 0; i--) {
-            T item = RIGHT_STACK.remove(i);
-            LEFT_STACK.add(item);
-        }
+        if (REVERSED.isEmpty()) pour();
+        return REVERSED.peek();
     }
 }
