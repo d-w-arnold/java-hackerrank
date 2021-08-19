32794c307728a9c5249f2b418380f2ddf7aea03c
David W. Arnold <d.w.arnold.gm@gmail.com>
2021-08-19 12:54:23 +0100

    Started minimumMoves() solution

---
+++ src/InterviewPreparationKit/StacksAndQueues/Medium/CastleOnTheGrid.java
@@ -0,0 +1,25 @@
+package InterviewPreparationKit.StacksAndQueues.Medium;
+
+import java.util.List;
+
+/**
+ * @author David W. Arnold
+ * @version 19/08/2021
+ */
+public class CastleOnTheGrid
+{
+    /**
+     * Castle on the Grid problem: https://www.hackerrank.com/challenges/castle-on-the-grid/problem
+     *
+     * @param grid   An array of strings that represent the rows of the grid.
+     * @param startX Starting X coordinate.
+     * @param startY Starting Y coordinate.
+     * @param goalX  Ending X coordinate.
+     * @param goalY  Ending Y coordinate.
+     * @return The minimum moves to reach the goal.
+     */
+    public static int minimumMoves(List<String> grid, int startX, int startY, int goalX, int goalY)
+    {
+        return -2;
+    }
+}
---
+++ tests/InterviewPreparationKit/StacksAndQueues/Medium/CastleOnTheGridTest.java
@@ -0,0 +1,45 @@
+package InterviewPreparationKit.StacksAndQueues.Medium;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author David W. Arnold
+ * @version 19/08/2021
+ */
+class CastleOnTheGridTest
+{
+
+    @BeforeEach
+    void setUp()
+    {
+    }
+
+    @AfterEach
+    void tearDown()
+    {
+    }
+
+    @Test
+    void minimumMoves_1()
+    {
+        assertEquals(2, CastleOnTheGrid.minimumMoves(
+                Arrays.asList("...", ".X.", "..."),
+                0, 0, 1, 2
+        ));
+    }
+
+    @Test
+    void minimumMoves_2()
+    {
+        assertEquals(3, CastleOnTheGrid.minimumMoves(
+                Arrays.asList(".X.", ".X.", "..."),
+                0, 0, 0, 2
+        ));
+    }
+}
