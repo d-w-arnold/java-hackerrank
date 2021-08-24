0345ae9fc420a24f05d8703408b8e7f07821bb80
David W. Arnold <d.w.arnold.gm@gmail.com>
2021-08-24 14:30:15 +0100

    Reverted minimumMoves() solution to third attempt

--- src/InterviewPreparationKit/StacksAndQueues/Medium/CastleOnTheGrid.java
+++ src/InterviewPreparationKit/StacksAndQueues/Medium/CastleOnTheGrid.java
@@ -10,7 +10,6 @@ public class CastleOnTheGrid
 {
     /**
      * Castle on the Grid problem: https://www.hackerrank.com/challenges/castle-on-the-grid/problem
-     * TODO: Achieve 100% score on Hackerrank, 4x tests failing due to timeout.
      *
      * @param grid   An array of strings that represent the rows of the grid.
      * @param startX Starting X coordinate.
@@ -21,104 +20,130 @@ public class CastleOnTheGrid
      */
     public static int minimumMoves(List<String> grid, int startX, int startY, int goalX, int goalY)
     {
-        char[][] gridArray = getGridArray(grid);
-        GridXY curr = new GridXY(startX, startY);
-        GridXY end = new GridXY(goalX, goalY);
-        Set<GridXY> visited = new HashSet<>();
-        visited.add(curr);
-        Queue<GridXY> queue = new LinkedList<>();
-        queue.add(curr);
-        Queue<Integer> step = new LinkedList<>();
-        step.add(0);
+        int minimumMoves = -1;
+        if (grid.get(startX).charAt(startY) == 'X' || grid.get(goalX).charAt(goalY) == 'X') return minimumMoves;
+        final Map<Character, Character> oppMoves = new HashMap<>();
+        oppMoves.put('L', 'R');
+        oppMoves.put('R', 'L');
+        oppMoves.put('U', 'D');
+        oppMoves.put('D', 'U');
+        final Set<String> blockedCells = getBlockedCells(grid);
+        List<Integer[]> queue = simulateMoves(grid, oppMoves, blockedCells, new Integer[]{0, (int) 'F', goalX, goalY}, startX, startY);
         while (!queue.isEmpty()) {
-            GridXY nextGridXY = queue.remove();
-            int hop = step.remove();
-            if (nextGridXY.equals(end)) return hop;
-            List<GridXY> next = getNext(gridArray, nextGridXY, visited);
-            visited.addAll(next);
-            queue.addAll(next);
-            for (int i = 0; i < next.size(); i++) {
-                step.add(hop + 1);
+            Integer[] entry = queue.remove(0);
+            if (minimumMoves == -1 || entry[0] < minimumMoves) {
+                if (startX == entry[2] && startY == entry[3]) {
+                    minimumMoves = entry[0];
+                } else queue.addAll(simulateMoves(grid, oppMoves, blockedCells, entry, goalX, goalY));
             }
         }
-        return Integer.MAX_VALUE;
+        return minimumMoves;
     }
 
-    private static char[][] getGridArray(List<String> grid)
+    private static Set<String> getBlockedCells(List<String> grid)
     {
-        char[][] array = new char[grid.size()][grid.get(0).length()];
+        Set<String> blockedCells = new HashSet<>();
         for (int i = 0; i < grid.size(); i++) {
             String row = grid.get(i);
             for (int j = 0; j < row.length(); j++) {
-                array[i][j] = row.charAt(j);
+                if (row.charAt(j) == 'X') blockedCells.add(i + "-" + j);
             }
         }
-        return array;
+        return blockedCells;
     }
 
-    private static List<GridXY> getNext(char[][] grid, GridXY curr, Set<GridXY> visited)
+    private static List<Integer[]> simulateMoves(List<String> grid, Map<Character, Character> oppMoves, Set<String> blockedCells, Integer[] queueEntry, int startX, int startY)
     {
-        List<GridXY> next = new ArrayList<>();
-        if (curr.dir == null || curr.dir == Dir.Y) {
-            int x = curr.x;
-            for (int i = x - 1; i >= 0; i--) {
-                if (grid[i][curr.y] == 'X') break;
-                final GridXY xy = new GridXY(i, curr.y, Dir.X);
-                if (!visited.contains(xy)) next.add(xy);
-            }
-            for (int i = x + 1; i < grid.length; i++) {
-                if (grid[i][curr.y] == 'X') break;
-                final GridXY xy = new GridXY(i, curr.y, Dir.X);
-                if (!visited.contains(xy)) next.add(xy);
+        List<Integer[]> possibleMoves = new ArrayList<>();
+        int currX = queueEntry[2];
+        int currY = queueEntry[3];
+        List<Character> movesOrder = new ArrayList<>();
+        for (char c : getMovesOrder(grid, oppMoves, currX, currY, startX - currX, startY - currY)) {
+            if (validMove(grid, oppMoves, blockedCells, c, queueEntry[1], currX, currY)) movesOrder.add(c);
+        }
+        Integer[] tmpQueueEntry;
+        for (char c : movesOrder) {
+            tmpQueueEntry = Arrays.copyOf(queueEntry, queueEntry.length);
+            if (c == 'L') {
+                tmpQueueEntry[3] -= 1;
+                possibleMoves.add(simulateMove('L', tmpQueueEntry));
+            } else if (c == 'R') {
+                tmpQueueEntry[3] += 1;
+                possibleMoves.add(simulateMove('R', tmpQueueEntry));
+            } else if (c == 'U') {
+                tmpQueueEntry[2] -= 1;
+                possibleMoves.add(simulateMove('U', tmpQueueEntry));
+            } else if (c == 'D') {
+                tmpQueueEntry[2] += 1;
+                possibleMoves.add(simulateMove('D', tmpQueueEntry));
             }
         }
-        if (curr.dir == null || curr.dir == Dir.X) {
-            int y = curr.y;
-            for (int i = y - 1; i >= 0; i--) {
-                if (grid[curr.x][i] == 'X') break;
-                final GridXY xy = new GridXY(curr.x, i, Dir.Y);
-                if (!visited.contains(xy)) next.add(xy);
-            }
-            for (int i = y + 1; i < grid.length; i++) {
-                if (grid[curr.x][i] == 'X') break;
-                final GridXY gridXy = new GridXY(curr.x, i, Dir.Y);
-                if (!visited.contains(gridXy)) next.add(gridXy);
-            }
-        }
-        return next;
+        return possibleMoves;
     }
 
-    private enum Dir
+    private static List<Character> getMovesOrder(List<String> grid, Map<Character, Character> oppMoves, int currX, int currY, int diffX, int diffY)
     {
-        X, Y
+        List<Character> moves = new ArrayList<>();
+        if (diffX != 0 && diffY != 0) {
+            if (diffX < 0 && diffY < 0) {
+                moves.add('U');
+                moves.add('L');
+            } else if (diffX < 0 && diffY > 0) {
+                moves.add('U');
+                moves.add('R');
+            } else if (diffX > 0 && diffY < 0) {
+                moves.add('D');
+                moves.add('L');
+            } else if (diffX > 0 && diffY > 0) {
+                moves.add('D');
+                moves.add('R');
+            }
+            if (Math.abs(diffX) < Math.abs(diffY)) Collections.reverse(moves);
+            for (int i = new ArrayList<>(moves).size() - 1; i >= 0; i--) {
+                moves.add(oppMoves.get(moves.get(i)));
+            }
+        } else if (diffX != 0) {
+            moves.add(diffX < 0 ? 'U' : 'D');
+            moves.add(currY > ((grid.get(0).length() - 1) / 2) ? 'L' : 'R');
+            moves.add(currY > ((grid.get(0).length() - 1) / 2) ? 'R' : 'L');
+            moves.add(diffX < 0 ? 'D' : 'U');
+        } else if (diffY != 0) {
+            moves.add(diffY < 0 ? 'L' : 'R');
+            moves.add(currX > ((grid.size() - 1) / 2) ? 'U' : 'D');
+            moves.add(currX > ((grid.size() - 1) / 2) ? 'D' : 'U');
+            moves.add(diffY < 0 ? 'R' : 'L');
+        }
+        return moves;
     }
 
-    private static class GridXY
+    private static boolean validMove(List<String> grid, Map<Character, Character> oppMoves, Set<String> blockedCells, char direction, int pastMove, int currX, int currY)
     {
-        int x, y;
-        Dir dir;
+        return pastMove != oppMoves.get(direction) && isInbounds(grid, direction, currX, currY) &&
+                !blockedCells.contains(getAdj(direction, currX, currY));
+    }
 
-        public GridXY(int x, int y)
-        {
-            this.x = x;
-            this.y = y;
-            dir = null;
-        }
+    private static boolean isInbounds(List<String> grid, char direction, int currX, int currY)
+    {
+        if (direction == 'L') return currY - 1 >= 0;
+        else if (direction == 'R') return currY + 1 < grid.get(currX).length();
+        else if (direction == 'U') return currX - 1 >= 0;
+        else if (direction == 'D') return currX + 1 < grid.size();
+        return false;
+    }
 
-        public GridXY(int x, int y, Dir dir)
-        {
-            this.x = x;
-            this.y = y;
-            this.dir = dir;
-        }
+    private static String getAdj(char direction, int currX, int currY)
+    {
+        if (direction == 'L') return currX + "-" + (currY - 1);
+        else if (direction == 'R') return currX + "-" + (currY + 1);
+        else if (direction == 'U') return (currX - 1) + "-" + currY;
+        else if (direction == 'D') return (currX + 1) + "-" + currY;
+        return null;
+    }
 
-        @Override
-        public boolean equals(Object o)
-        {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            GridXY gridXy = (GridXY) o;
-            return x == gridXy.x && y == gridXy.y;
-        }
+    private static Integer[] simulateMove(char direction, Integer[] queueEntry)
+    {
+        int newPastMoves = queueEntry[0];
+        if (queueEntry[1] != (int) direction) newPastMoves++;
+        return new Integer[]{newPastMoves, (int) direction, queueEntry[2], queueEntry[3]};
     }
 }
