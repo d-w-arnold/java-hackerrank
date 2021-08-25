82cf0cdfe6f7404849c5aa40d8d03a452ab961fa
David W. Arnold <d.w.arnold.gm@gmail.com>
2021-08-25 17:25:37 +0100

    Added first attempt riddle() solution

--- src/InterviewPreparationKit/StacksAndQueues/Medium/MinMaxRiddle.java
+++ src/InterviewPreparationKit/StacksAndQueues/Medium/MinMaxRiddle.java
@@ -1,5 +1,8 @@
 package InterviewPreparationKit.StacksAndQueues.Medium;
 
+import java.util.Arrays;
+import java.util.Stack;
+
 /**
  * @author David W. Arnold
  * @version 24/08/2021
@@ -8,13 +11,48 @@ public class MinMaxRiddle
 {
     /**
      * Min Max Riddle problem: https://www.hackerrank.com/challenges/min-max-riddle/problem
-     * TODO: Complete solution.
      *
      * @param arr An array of 'n' integers.
      * @return An array of integers representing the maximum minimum value for each window size from 1 to 'n'.
      */
     public static long[] riddle(long[] arr)
     {
-        return new long[]{-2};
+        int n = arr.length;
+        Stack<Integer> s = new Stack<>();
+        int[] left = new int[n + 1];
+        int[] right = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            left[i] = -1;
+            right[i] = n;
+        }
+        for (int i = 0; i < n; i++) {
+            while (!s.empty() && arr[s.peek()] >= arr[i]) {
+                s.pop();
+            }
+            if (!s.empty()) left[i] = s.peek();
+            s.push(i);
+        }
+        while (!s.empty()) {
+            s.pop();
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            while (!s.empty() && arr[s.peek()] >= arr[i]) {
+                s.pop();
+            }
+            if (!s.empty()) right[i] = s.peek();
+            s.push(i);
+        }
+        long[] maxMin = new long[n + 1];
+        for (int i = 0; i <= n; i++) {
+            maxMin[i] = 0;
+        }
+        for (int i = 0; i < n; i++) {
+            int len = right[i] - left[i] - 1;
+            maxMin[len] = Math.max(maxMin[len], arr[i]);
+        }
+        for (int i = n - 1; i >= 1; i--) {
+            maxMin[i] = Math.max(maxMin[i], maxMin[i + 1]);
+        }
+        return Arrays.copyOfRange(maxMin, 1, maxMin.length);
     }
 }
--- tests/InterviewPreparationKit/StacksAndQueues/Medium/MinMaxRiddleTest.java
+++ tests/InterviewPreparationKit/StacksAndQueues/Medium/MinMaxRiddleTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 /**
  * @author David W. Arnold
@@ -26,24 +26,24 @@ class MinMaxRiddleTest
     @Test
     void riddle_1()
     {
-        assertEquals(new long[]{12, 3, 3, 1, 1}, MinMaxRiddle.riddle(new long[]{6, 3, 5, 1, 12}));
+        assertArrayEquals(new long[]{12, 3, 3, 1, 1}, MinMaxRiddle.riddle(new long[]{6, 3, 5, 1, 12}));
     }
 
     @Test
     void riddle_2()
     {
-        assertEquals(new long[]{12, 2, 1, 1}, MinMaxRiddle.riddle(new long[]{2, 6, 1, 12}));
+        assertArrayEquals(new long[]{12, 2, 1, 1}, MinMaxRiddle.riddle(new long[]{2, 6, 1, 12}));
     }
 
     @Test
     void riddle_3()
     {
-        assertEquals(new long[]{13, 3, 2, 1, 1, 1, 1}, MinMaxRiddle.riddle(new long[]{1, 2, 3, 5, 1, 13, 3}));
+        assertArrayEquals(new long[]{13, 3, 2, 1, 1, 1, 1}, MinMaxRiddle.riddle(new long[]{1, 2, 3, 5, 1, 13, 3}));
     }
 
     @Test
     void riddle_4()
     {
-        assertEquals(new long[]{7, 6, 4, 4, 3, 2}, MinMaxRiddle.riddle(new long[]{3, 5, 4, 7, 6, 2}));
+        assertArrayEquals(new long[]{7, 6, 4, 4, 3, 2}, MinMaxRiddle.riddle(new long[]{3, 5, 4, 7, 6, 2}));
     }
 }
