4551e1b46db997de31e8a3b288fa086eb3f6164a
David W. Arnold <d.w.arnold.gm@gmail.com>
2021-08-31 16:26:56 +0100

    Added first attempt findShortest() solution

--- src/InterviewPreparationKit/Graphs/Medium/FindTheNearestClone.java
+++ src/InterviewPreparationKit/Graphs/Medium/FindTheNearestClone.java
@@ -1,5 +1,7 @@
 package InterviewPreparationKit.Graphs.Medium;
 
+import java.util.*;
+
 /**
  * @author David W. Arnold
  * @version 27/08/2021
@@ -18,6 +20,49 @@ public class FindTheNearestClone
      */
     public static int findShortest(int graphNodes, int[] graphFrom, int[] graphTo, long[] ids, int val)
     {
-        return -2;
+        Map<Long, List<Long>> colourCounts = new HashMap<>();
+        for (int i = 0; i < graphNodes; i++) {
+            long c = ids[i];
+            if (colourCounts.containsKey(c)) colourCounts.get(c).add((long) (i + 1));
+            else colourCounts.put(c, new ArrayList<>(Collections.singletonList((long) i + 1)));
+        }
+        if (colourCounts.containsKey((long) val) && colourCounts.get((long) val).size() == 1) return -1;
+        Map<Long, List<Long>> paths = new HashMap<>();
+        for (int i = 0; i < graphFrom.length; i++) {
+            long from = graphFrom[i];
+            long to = graphTo[i];
+            if (paths.containsKey(from)) paths.get(from).add(to);
+            else paths.put(from, new ArrayList<>(Collections.singletonList(to)));
+            if (paths.containsKey(to)) paths.get(to).add(from);
+            else paths.put(to, new ArrayList<>(Collections.singletonList(from)));
+        }
+        long mostPaths = -1;
+        for (long i : colourCounts.get((long) val)) {
+            mostPaths = mostPaths != -1 ? Math.max(mostPaths, paths.get(i).size()) : i;
+        }
+        Set<Long> colourNodes = new HashSet<>(colourCounts.get((long) val));
+        colourNodes.remove(mostPaths);
+        Map<String, Long> possiblePaths = new HashMap<>();
+        Set<Long> visited = new HashSet<>(new ArrayList<>(Collections.singletonList(mostPaths)));
+        Queue<Long[]> queue = new LinkedList<>();
+        for (long l : paths.get(mostPaths)) {
+            queue.add(new Long[]{l, 1L});
+            visited.add(l);
+        }
+        while (!queue.isEmpty()) {
+            Long[] entry = queue.remove();
+            if (colourNodes.contains(entry[0])) possiblePaths.put(mostPaths + "-" + entry[0], entry[1]);
+            for (long l : paths.get(entry[0])) {
+                if (!visited.contains(l)) {
+                    queue.add(new Long[]{l, entry[1] + 1});
+                    visited.add(l);
+                }
+            }
+        }
+        long shortestPath = -1;
+        for (Map.Entry<String, Long> entry : possiblePaths.entrySet()) {
+            shortestPath = shortestPath != -1 ? Math.min(shortestPath, entry.getValue()) : entry.getValue();
+        }
+        return (int) shortestPath;
     }
 }
