a44fe53d1c260301a767f329fbb1dd14a0f67337
David W. Arnold <d.w.arnold.gm@gmail.com>
2021-09-21 10:37:41 +0100

    Started fibonacci() solution

---
+++ src/main/java/InterviewPreparationKit/RecursionAndBacktracking/FibonacciNumbers.java
@@ -0,0 +1,19 @@
+package InterviewPreparationKit.RecursionAndBacktracking;
+
+/**
+ * @author David W. Arnold
+ * @version 21/09/2021
+ */
+public class FibonacciNumbers
+{
+    /**
+     * https://www.hackerrank.com/challenges/ctci-fibonacci-numbers/problem
+     *
+     * @param n The index of the sequence to return.
+     * @return The nth element in the Fibonacci sequence.
+     */
+    public static int fibonacci(int n)
+    {
+        return -2;
+    }
+}
---
+++ src/test/java/InterviewPreparationKit/RecursionAndBacktracking/FibonacciNumbersTest.java
@@ -0,0 +1,43 @@
+package InterviewPreparationKit.RecursionAndBacktracking;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author David W. Arnold
+ * @version 21/09/2021
+ */
+class FibonacciNumbersTest
+{
+
+    @BeforeEach
+    void setUp()
+    {
+    }
+
+    @AfterEach
+    void tearDown()
+    {
+    }
+
+//    @Test
+//    void fibonacci_1()
+//    {
+//        assertEquals(5, FibonacciNumbers.fibonacci(5));
+//    }
+//
+//    @Test
+//    void fibonacci_2()
+//    {
+//        assertEquals(144, FibonacciNumbers.fibonacci(12));
+//    }
+//
+//    @Test
+//    void fibonacci_3()
+//    {
+//        assertEquals(1597, FibonacciNumbers.fibonacci(17));
+//    }
+}
