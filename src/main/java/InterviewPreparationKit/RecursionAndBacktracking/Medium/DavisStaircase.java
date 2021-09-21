94ea31473f99082f2915325dfea866446e72cfef
David W. Arnold <d.w.arnold.gm@gmail.com>
2021-09-21 11:42:15 +0100

    Added stepPerms() solution

--- src/main/java/InterviewPreparationKit/RecursionAndBacktracking/Medium/DavisStaircase.java
+++ src/main/java/InterviewPreparationKit/RecursionAndBacktracking/Medium/DavisStaircase.java
@@ -1,5 +1,8 @@
 package InterviewPreparationKit.RecursionAndBacktracking.Medium;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author David W. Arnold
  * @version 21/09/2021
@@ -14,6 +17,17 @@ public class DavisStaircase
      */
     public static int stepPerms(int n)
     {
-        return -2;
+        Map<Integer, Integer> cache = new HashMap<>();
+        return stepPermsHelper(cache, n);
+    }
+
+    private static int stepPermsHelper(Map<Integer, Integer> cache, int n)
+    {
+        if (n == 1) return 1;
+        else if (n == 2) return 2;
+        else if (n == 3) return 4;
+        else if (!cache.containsKey(n))
+            cache.put(n, stepPermsHelper(cache, n - 1) + stepPermsHelper(cache, n - 2) + stepPermsHelper(cache, n - 3));
+        return cache.get(n);
     }
 }
