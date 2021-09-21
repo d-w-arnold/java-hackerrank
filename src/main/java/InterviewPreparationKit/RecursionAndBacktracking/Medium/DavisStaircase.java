2bdd860d3fb82786d3bb6b4bb5b6ea42a02e67b8
David W. Arnold <d.w.arnold.gm@gmail.com>
2021-09-21 11:33:16 +0100

    Started stepPerms() solution

---
+++ src/main/java/InterviewPreparationKit/RecursionAndBacktracking/Medium/DavisStaircase.java
@@ -0,0 +1,19 @@
+package InterviewPreparationKit.RecursionAndBacktracking.Medium;
+
+/**
+ * @author David W. Arnold
+ * @version 21/09/2021
+ */
+public class DavisStaircase
+{
+    /**
+     * Recursion: Davis' Staircase problem: https://www.hackerrank.com/challenges/ctci-recursive-staircase/problem
+     *
+     * @param n The number of stairs in the staircase.
+     * @return The number of ways Davis can climb the staircase, modulo 10000000007.
+     */
+    public static int stepPerms(int n)
+    {
+        return -2;
+    }
+}
---
+++ src/test/java/InterviewPreparationKit/RecursionAndBacktracking/Medium/DavisStaircaseTest.java
@@ -0,0 +1,73 @@
+package InterviewPreparationKit.RecursionAndBacktracking.Medium;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author David W. Arnold
+ * @version 21/09/2021
+ */
+class DavisStaircaseTest
+{
+
+    @BeforeEach
+    void setUp()
+    {
+    }
+
+    @AfterEach
+    void tearDown()
+    {
+    }
+
+//    @Test
+//    void stepPerms_1()
+//    {
+//        assertEquals(1, DavisStaircase.stepPerms(1));
+//    }
+//
+//    @Test
+//    void stepPerms_2()
+//    {
+//        assertEquals(4, DavisStaircase.stepPerms(3));
+//    }
+//
+//    @Test
+//    void stepPerms_3()
+//    {
+//        assertEquals(44, DavisStaircase.stepPerms(7));
+//    }
+//
+//    @Test
+//    void stepPerms_4()
+//    {
+//        assertEquals(13, DavisStaircase.stepPerms(5));
+//    }
+//
+//    @Test
+//    void stepPerms_5()
+//    {
+//        assertEquals(81, DavisStaircase.stepPerms(8));
+//    }
+//
+//    @Test
+//    void stepPerms_6()
+//    {
+//        assertEquals(5768, DavisStaircase.stepPerms(15));
+//    }
+//
+//    @Test
+//    void stepPerms_7()
+//    {
+//        assertEquals(121415, DavisStaircase.stepPerms(20));
+//    }
+//
+//    @Test
+//    void stepPerms_8()
+//    {
+//        assertEquals(8646064, DavisStaircase.stepPerms(27));
+//    }
+}
