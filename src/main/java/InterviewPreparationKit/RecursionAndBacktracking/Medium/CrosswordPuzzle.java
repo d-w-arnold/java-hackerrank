60c56532b12c003f75d93568a2236c03a404d8c4
David W. Arnold <d.w.arnold.gm@gmail.com>
2021-09-22 14:54:04 +0100

    Started crosswordPuzzle() solution

---
+++ src/main/java/InterviewPreparationKit/RecursionAndBacktracking/Medium/CrosswordPuzzle.java
@@ -0,0 +1,23 @@
+package InterviewPreparationKit.RecursionAndBacktracking.Medium;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author David W. Arnold
+ * @version 22/09/2021
+ */
+public class CrosswordPuzzle
+{
+    /**
+     * Crossword Puzzle problem: https://www.hackerrank.com/challenges/crossword-puzzle/problem
+     *
+     * @param crossword An array of 10 strings of length 10 representing the empty grid.
+     * @param words     A string consisting of semicolon delimited strings to fit into crossword.
+     * @return An array of strings, each representing a row of the finished puzzle.
+     */
+    public static List<String> crosswordPuzzle(List<String> crossword, String words)
+    {
+        return Arrays.asList("NULL");
+    }
+}
