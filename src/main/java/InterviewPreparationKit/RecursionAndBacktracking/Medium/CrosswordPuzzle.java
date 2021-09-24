f60b912cccb4f933dcd6ea95e46ff43abcb1331b
David W. Arnold <d.w.arnold.gm@gmail.com>
2021-09-24 15:47:24 +0100

    Added third attempt crosswordPuzzle() solution

--- src/main/java/InterviewPreparationKit/RecursionAndBacktracking/Medium/CrosswordPuzzle.java
+++ src/main/java/InterviewPreparationKit/RecursionAndBacktracking/Medium/CrosswordPuzzle.java
@@ -24,34 +24,33 @@ public class CrosswordPuzzle
 
     static class Puzzle
     {
-        private final char blank = '-';
-        private final char nonBlank = '+';
-        private final String sep = ":";
-        private final char[][] crossword;
-        private final Set<String> words;
-        private final Stack<String> usedWords;
-        private final Map<Integer, Set<String>> wordsMap; // Word length -> List of words of that length
-        private final Map<Integer, Integer[]> blankWords;
-        private final Map<Integer, String> nonBlankWords;
-        private final Map<Integer, Set<String>> blankWordPossibilities;
-        private final Queue<Integer> blankWordQueue;
-        private final Map<String, Character> blankCells; // x:y -> character in cell
-        private final Stack<String> updatedCells;
+        private final char BLANK = '-';
+        private final String SEP = ":";
+        private final char[][] CROSSWORD;
+        private final Map<Integer, Integer[]> BLANK_WORDS;
+        private final Queue<Integer> BLANK_WORDS_QUEUE;
+        private final Set<List<String>> PERMS;
+        private Set<String> words;
+        private Map<Integer, Set<String>> wordsMap; // Word length -> List of words of that length
+        private Stack<String> usedWords;
+        private Map<Integer, String> nonBlankWords;
+        private Map<String, Character> blankCells; // x:y -> Character in cell
         private Map<Integer, String> blankWordTemplates;
+        private Map<Integer, Set<String>> blankWordPossibilities;
 
         public Puzzle(List<String> crossword, String words)
         {
-            this.crossword = new char[crossword.size()][crossword.get(0).length()];
+            this.CROSSWORD = new char[crossword.size()][crossword.get(0).length()];
+            this.BLANK_WORDS = new HashMap<>();
+            this.BLANK_WORDS_QUEUE = new LinkedList<>();
+            this.PERMS = new HashSet<>();
             this.words = new HashSet<>(Arrays.asList(words.split(";")));
             this.wordsMap = new HashMap<>();
             this.usedWords = new Stack<>();
-            this.blankWords = new HashMap<>();
             this.nonBlankWords = new HashMap<>();
-            this.blankWordPossibilities = new HashMap<>();
-            this.blankWordQueue = new LinkedList<>();
             this.blankCells = new HashMap<>();
-            this.updatedCells = new Stack<>();
-            this.blankWordTemplates = new HashMap<>();
+            this.blankWordTemplates = new LinkedHashMap<>();
+            this.blankWordPossibilities = new HashMap<>();
             getWordsAndWordsMap();
             getBlankWordsAndCells(crossword);
         }
@@ -75,49 +74,49 @@ public class CrosswordPuzzle
             for (int i = 0; i < crossword.size(); i++) {
                 for (int j = 0; j < crossword.get(i).length(); j++) {
                     c = crossword.get(i).charAt(j);
-                    this.crossword[i][j] = c;
-                    if (c == blank) {
-                        xSepY = i + sep + j;
-                        blankCells.put(xSepY, blank);
-                        xSepYSepRight = xSepY + sep + 0;
-                        xSepYSepDown = xSepY + sep + 1;
+                    this.CROSSWORD[i][j] = c;
+                    if (c == BLANK) {
+                        xSepY = i + SEP + j;
+                        blankCells.put(xSepY, BLANK);
+                        xSepYSepRight = xSepY + SEP + 0;
+                        xSepYSepDown = xSepY + SEP + 1;
                         containsRight = false;
                         containsDown = false;
                         cRight = crossword.get(i).charAt(j + 1);
-                        cDown = i + 1 < crossword.size() ? crossword.get(i + 1).charAt(j) : nonBlank;
+                        cDown = i + 1 < crossword.size() ? crossword.get(i + 1).charAt(j) : '+';
                         if (cache.containsKey(xSepYSepRight) || cache.containsKey(xSepYSepDown)) {
                             if (cache.containsKey(xSepYSepRight)) {
                                 containsRight = true;
-                                if (cRight == blank) {
+                                if (cRight == BLANK) {
                                     Integer[] value = cache.get(xSepYSepRight);
                                     value[3]++;
-                                    cache.put(i + sep + (j + 1) + sep + 0, value);
+                                    cache.put(i + SEP + (j + 1) + SEP + 0, value);
                                 } else {
-                                    blankWords.put(index, cache.get(xSepYSepRight));
+                                    BLANK_WORDS.put(index, cache.get(xSepYSepRight));
                                     index++;
                                 }
                                 cache.remove(xSepYSepRight);
                             }
                             if (cache.containsKey(xSepYSepDown)) {
                                 containsDown = true;
-                                if (cDown == blank) {
+                                if (cDown == BLANK) {
                                     Integer[] value = cache.get(xSepYSepDown);
                                     value[3]++;
-                                    cache.put((i + 1) + sep + j + sep + 1, value);
+                                    cache.put((i + 1) + SEP + j + SEP + 1, value);
                                 } else {
-                                    blankWords.put(index, cache.get(xSepYSepDown));
+                                    BLANK_WORDS.put(index, cache.get(xSepYSepDown));
                                     index++;
                                 }
                                 cache.remove(xSepYSepDown);
                             }
                         }
-                        if (cRight == blank || cDown == blank) {
-                            if (cRight == blank && !containsRight)
-                                cache.put(i + sep + (j + 1) + sep + 0, new Integer[]{i, j, 0, 2});
-                            if (cDown == blank && !containsDown)
-                                cache.put((i + 1) + sep + j + sep + 1, new Integer[]{i, j, 1, 2});
+                        if (cRight == BLANK || cDown == BLANK) {
+                            if (cRight == BLANK && !containsRight)
+                                cache.put(i + SEP + (j + 1) + SEP + 0, new Integer[]{i, j, 0, 2});
+                            if (cDown == BLANK && !containsDown)
+                                cache.put((i + 1) + SEP + j + SEP + 1, new Integer[]{i, j, 1, 2});
                         } else if (!containsRight && !containsDown) {
-                            blankWords.put(index, new Integer[]{i, j, 0, 1});
+                            BLANK_WORDS.put(index, new Integer[]{i, j, 0, 1});
                             index++;
                         }
                     }
@@ -128,93 +127,144 @@ public class CrosswordPuzzle
         public void insertWords()
         {
             insertWordsOfUniqueLength();
-            getBlankWordTemplates();
+            getBlankWordTemplatesAndPossibilities();
             List<String> matches;
-            String blankWordTemplate;
+            String blankWordTemplate, word;
             Integer[] i;
-            int wordLen;
-            while (!blankWordQueue.isEmpty()) {
-                int entry = blankWordQueue.remove();
+            int entry;
+            while (!BLANK_WORDS_QUEUE.isEmpty()) {
+                entry = BLANK_WORDS_QUEUE.remove();
                 matches = new ArrayList<>();
                 blankWordTemplate = blankWordTemplates.get(entry);
-                for (String word : words) {
-                    if (matchesTemplate(word, blankWordTemplate)) matches.add(word);
+                for (String w : words) {
+                    if (matchesTemplate(w, blankWordTemplate)) matches.add(w);
                 }
                 if (matches.size() == 1) {
-                    String word = matches.get(0);
-                    i = blankWords.get(entry);
-                    wordLen = i[3];
-                    updateBlankCells(word, i, wordLen);
-                    words.remove(word);
-                    usedWords.add(word);
-                    wordsMap.get(wordLen).remove(word);
-                    nonBlankWords.put(entry, word);
-                    blankWordPossibilities.remove(entry);
-                    blankWordTemplates.remove(entry);
+                    word = matches.get(0);
+                    i = BLANK_WORDS.get(entry);
+                    updateBlankCells(word, i, entry);
+                    getBlankWordTemplatesAndPossibilities();
                 } else if (matches.size() > 1) blankWordPossibilities.get(entry).addAll(matches);
             }
             // TODO: Brute force any multiple matches for a given blankWord slot.
-            System.out.println();
-            for (Map.Entry<String, Character> entry : blankCells.entrySet()) {
-                String[] s = entry.getKey().split(sep);
-                crossword[Integer.parseInt(s[0])][Integer.parseInt(s[1])] = entry.getValue();
+            permute(new ArrayList<>(words));
+            Map<String, Character> tmpBlankCells = new HashMap<>(blankCells);
+            Set<String> tmpWords = new HashSet<>(words);
+            Stack<String> tmpUsedWords = new Stack<>();
+            tmpUsedWords.addAll(usedWords);
+            Map<Integer, Set<String>> tmpWordsMap = new HashMap<>(wordsMap);
+            Map<Integer, String> tmpNonBlankWords = new HashMap<>(nonBlankWords);
+            Map<Integer, Set<String>> tmpBlankWordPossibilities = new HashMap<>(blankWordPossibilities);
+            Map<Integer, String> tmpBlankWordTemplates = new HashMap<>(blankWordTemplates);
+            int index;
+            nextPerm:
+            for (List<String> perm : PERMS) {
+                index = 0;
+                getBlankWordTemplatesAndPossibilities();
+                while (!BLANK_WORDS_QUEUE.isEmpty()) {
+                    entry = BLANK_WORDS_QUEUE.remove();
+                    blankWordTemplate = blankWordTemplates.get(entry);
+                    word = perm.get(index);
+                    if (matchesTemplate(word, blankWordTemplate)) {
+                        i = BLANK_WORDS.get(entry);
+                        updateBlankCells(word, i, entry);
+                        getBlankWordTemplatesAndPossibilities();
+                        index++;
+                    } else {
+                        this.blankCells = new HashMap<>(tmpBlankCells);
+                        this.words = new HashSet<>(tmpWords);
+                        this.usedWords = new Stack<>();
+                        this.usedWords.addAll(tmpUsedWords);
+                        this.wordsMap = new HashMap<>(tmpWordsMap);
+                        this.nonBlankWords = new HashMap<>(tmpNonBlankWords);
+                        this.blankWordPossibilities = new HashMap<>(tmpBlankWordPossibilities);
+                        this.blankWordTemplates = new LinkedHashMap<>(tmpBlankWordTemplates);
+                        break nextPerm;
+                    }
+                }
             }
+            for (Map.Entry<String, Character> e : blankCells.entrySet()) {
+                String[] s = e.getKey().split(SEP);
+                CROSSWORD[Integer.parseInt(s[0])][Integer.parseInt(s[1])] = e.getValue();
+            }
+        }
+
+        private void permute(List<String> words)
+        {
+            permutation(0, words.size() - 1, words);
+        }
+
+        private void permutation(int start, int end, List<String> words)
+        {
+            if (start == end) PERMS.add(new ArrayList<>(words));
+            for (int i = start; i <= end; i++) {
+                PERMS.add(swap(words, start, i));
+                permutation(start + 1, end, words);
+                PERMS.add(swap(words, start, i));
+            }
+        }
+
+        private List<String> swap(List<String> arr, int a, int b)
+        {
+            if (a == b) return new ArrayList<>(arr);
+            String tmp = arr.get(b);
+            arr.set(b, arr.get(a));
+            arr.set(a, tmp);
+            return new ArrayList<>(arr);
         }
 
         private void insertWordsOfUniqueLength()
         {
-            String word;
             int wordLen;
-            for (int k = 0; k < blankWords.size(); k++) {
-                if (!nonBlankWords.containsKey(k) && blankWords.containsKey(k)) {
-                    Integer[] i = blankWords.get(k);
+            for (int k = 0; k < BLANK_WORDS.size(); k++) {
+                if (!nonBlankWords.containsKey(k) && BLANK_WORDS.containsKey(k)) {
+                    Integer[] i = BLANK_WORDS.get(k);
                     wordLen = i[3];
-                    if (wordsMap.containsKey(wordLen) && wordsMap.get(wordLen).size() == 1) {
-                        word = wordsMap.get(wordLen).iterator().next();
-                        updateBlankCells(word, i, wordLen);
-                        words.remove(word);
-                        usedWords.add(word);
-                        wordsMap.remove(wordLen);
-                        nonBlankWords.put(k, word);
-                    }
+                    if (wordsMap.containsKey(wordLen) && wordsMap.get(wordLen).size() == 1)
+                        updateBlankCells(wordsMap.get(wordLen).iterator().next(), i, k);
                 }
             }
         }
 
-        private void updateBlankCells(String word, Integer[] i, int wordLen)
+        private void updateBlankCells(String word, Integer[] i, int bwIndex)
         {
             int tmpX, tmpY;
-            for (int j = 0; j < wordLen; j++) {
+            for (int j = 0; j < i[3]; j++) {
                 tmpX = i[0];
                 tmpY = i[1];
                 if (i[2] == 0) tmpY += j;
                 else if (i[2] == 1) tmpX += j;
-                String xSepY = tmpX + sep + tmpY;
+                String xSepY = tmpX + SEP + tmpY;
                 blankCells.put(xSepY, word.charAt(j));
-                updatedCells.add(xSepY);
             }
+            words.remove(word);
+            usedWords.add(word);
+            wordsMap.get(i[3]).remove(word);
+            nonBlankWords.put(bwIndex, word);
         }
 
-        private void getBlankWordTemplates()
+        private void getBlankWordTemplatesAndPossibilities()
         {
-            blankWordTemplates = new HashMap<>();
+            this.blankWordTemplates = new LinkedHashMap<>();
+            this.blankWordPossibilities = new HashMap<>();
             Set<Integer> addedToQueue = new HashSet<>();
             Integer[] i;
             StringBuilder sb;
             int tmpX, tmpY;
             char c;
-            for (int j = 0; j < blankWords.size(); j++) {
-                if (!nonBlankWords.containsKey(j) && blankWords.containsKey(j)) {
-                    i = blankWords.get(j);
+            for (int j = 0; j < BLANK_WORDS.size(); j++) {
+                if (!nonBlankWords.containsKey(j) && BLANK_WORDS.containsKey(j)) {
+                    i = BLANK_WORDS.get(j);
                     sb = new StringBuilder();
                     for (int k = 0; k < i[3]; k++) {
                         tmpX = i[0];
                         tmpY = i[1];
                         if (i[2] == 0) tmpY += k;
                         else if (i[2] == 1) tmpX += k;
-                        c = blankCells.get(tmpX + sep + tmpY);
-                        if (c != blank && !addedToQueue.contains(j)) {
-                            blankWordQueue.add(j);
+                        c = blankCells.get(tmpX + SEP + tmpY);
+                        System.out.println();
+                        if (c != BLANK && !addedToQueue.contains(j)) {
+                            BLANK_WORDS_QUEUE.add(j);
                             addedToQueue.add(j);
                         }
                         sb.append(c);
@@ -231,7 +281,7 @@ public class CrosswordPuzzle
             char c;
             for (int i = 0; i < template.length(); i++) {
                 c = template.charAt(i);
-                if (c == blank) continue;
+                if (c == BLANK) continue;
                 if (c != word.charAt(i)) return false;
             }
             return true;
@@ -240,7 +290,7 @@ public class CrosswordPuzzle
         public List<String> printCrosswordPuzzle()
         {
             List<String> list = new ArrayList<>();
-            for (char[] row : crossword) {
+            for (char[] row : CROSSWORD) {
                 list.add(String.valueOf(row));
             }
             return list;
--- src/test/java/InterviewPreparationKit/RecursionAndBacktracking/Medium/CrosswordPuzzleTest.java
+++ src/test/java/InterviewPreparationKit/RecursionAndBacktracking/Medium/CrosswordPuzzleTest.java
@@ -26,127 +26,127 @@ class CrosswordPuzzleTest
     {
     }
 
-//    @Test
-//    void crosswordPuzzle_1()
-//    {
-//        List<String> start = Arrays.asList(
-//                "++++++++++",
-//                "+------+++",
-//                "+++-++++++",
-//                "+++-++++++",
-//                "+++-----++",
-//                "+++-++-+++",
-//                "++++++-+++",
-//                "++++++-+++",
-//                "++++++-+++",
-//                "++++++++++"
-//        );
-//        String words = "POLAND;LHASA;SPAIN;INDIA";
-//        List<String> expected = Arrays.asList(
-//                "++++++++++",
-//                "+POLAND+++",
-//                "+++H++++++",
-//                "+++A++++++",
-//                "+++SPAIN++",
-//                "+++A++N+++",
-//                "++++++D+++",
-//                "++++++I+++",
-//                "++++++A+++",
-//                "++++++++++"
-//        );
-//        assertEquals(expected, CrosswordPuzzle.crosswordPuzzle(start, words));
-//    }
-//
-//    @Test
-//    void crosswordPuzzle_2()
-//    {
-//        List<String> start = Arrays.asList(
-//                "+-++++++++",
-//                "+-++++++++",
-//                "+-++++++++",
-//                "+-----++++",
-//                "+-+++-++++",
-//                "+-+++-++++",
-//                "+++++-++++",
-//                "++------++",
-//                "+++++-++++",
-//                "+++++-++++"
-//        );
-//        String words = "LONDON;DELHI;ICELAND;ANKARA";
-//        List<String> expected = Arrays.asList(
-//                "+L++++++++",
-//                "+O++++++++",
-//                "+N++++++++",
-//                "+DELHI++++",
-//                "+O+++C++++",
-//                "+N+++E++++",
-//                "+++++L++++",
-//                "++ANKARA++",
-//                "+++++N++++",
-//                "+++++D++++"
-//        );
-//        assertEquals(expected, CrosswordPuzzle.crosswordPuzzle(start, words));
-//    }
-//
-//    @Test
-//    void crosswordPuzzle_3()
-//    {
-//        List<String> start = Arrays.asList(
-//                "+-++++++++",
-//                "+-++++++++",
-//                "+-------++",
-//                "+-++++++++",
-//                "+-++++++++",
-//                "+------+++",
-//                "+-+++-++++",
-//                "+++++-++++",
-//                "+++++-++++",
-//                "++++++++++"
-//        );
-//        String words = "AGRA;NORWAY;ENGLAND;GWALIOR";
-//        List<String> expected = Arrays.asList(
-//                "+E++++++++",
-//                "+N++++++++",
-//                "+GWALIOR++",
-//                "+L++++++++",
-//                "+A++++++++",
-//                "+NORWAY+++",
-//                "+D+++G++++",
-//                "+++++R++++",
-//                "+++++A++++",
-//                "++++++++++"
-//        );
-//        assertEquals(expected, CrosswordPuzzle.crosswordPuzzle(start, words));
-//    }
-//
-//    @Test
-//    void crosswordPuzzle_4()
-//    {
-//        List<String> start = Arrays.asList(
-//                "++++++-+++",
-//                "++------++",
-//                "++++++-+++",
-//                "++++++-+++",
-//                "+++------+",
-//                "++++++-+-+",
-//                "++++++-+-+",
-//                "++++++++-+",
-//                "++++++++-+",
-//                "++++++++-+"
-//        );
-//        String words = "ICELAND;MEXICO;PANAMA;ALMATY";
-//        List<String> expected = Arrays.asList(
-//                "++++++I+++",
-//                "++MEXICO++",
-//                "++++++E+++",
-//                "++++++L+++",
-//                "+++PANAMA+",
-//                "++++++N+L+",
-//                "++++++D+M+",
-//                "++++++++A+",
-//                "++++++++T+",
-//                "++++++++Y+"
-//        );
-//        assertEquals(expected, CrosswordPuzzle.crosswordPuzzle(start, words));
-//    }
+    @Test
+    void crosswordPuzzle_1()
+    {
+        List<String> start = Arrays.asList(
+                "++++++++++",
+                "+------+++",
+                "+++-++++++",
+                "+++-++++++",
+                "+++-----++",
+                "+++-++-+++",
+                "++++++-+++",
+                "++++++-+++",
+                "++++++-+++",
+                "++++++++++"
+        );
+        String words = "POLAND;LHASA;SPAIN;INDIA";
+        List<String> expected = Arrays.asList(
+                "++++++++++",
+                "+POLAND+++",
+                "+++H++++++",
+                "+++A++++++",
+                "+++SPAIN++",
+                "+++A++N+++",
+                "++++++D+++",
+                "++++++I+++",
+                "++++++A+++",
+                "++++++++++"
+        );
+        assertEquals(expected, CrosswordPuzzle.crosswordPuzzle(start, words));
+    }
+
+    @Test
+    void crosswordPuzzle_2()
+    {
+        List<String> start = Arrays.asList(
+                "+-++++++++",
+                "+-++++++++",
+                "+-++++++++",
+                "+-----++++",
+                "+-+++-++++",
+                "+-+++-++++",
+                "+++++-++++",
+                "++------++",
+                "+++++-++++",
+                "+++++-++++"
+        );
+        String words = "LONDON;DELHI;ICELAND;ANKARA";
+        List<String> expected = Arrays.asList(
+                "+L++++++++",
+                "+O++++++++",
+                "+N++++++++",
+                "+DELHI++++",
+                "+O+++C++++",
+                "+N+++E++++",
+                "+++++L++++",
+                "++ANKARA++",
+                "+++++N++++",
+                "+++++D++++"
+        );
+        assertEquals(expected, CrosswordPuzzle.crosswordPuzzle(start, words));
+    }
+
+    @Test
+    void crosswordPuzzle_3()
+    {
+        List<String> start = Arrays.asList(
+                "+-++++++++",
+                "+-++++++++",
+                "+-------++",
+                "+-++++++++",
+                "+-++++++++",
+                "+------+++",
+                "+-+++-++++",
+                "+++++-++++",
+                "+++++-++++",
+                "++++++++++"
+        );
+        String words = "AGRA;NORWAY;ENGLAND;GWALIOR";
+        List<String> expected = Arrays.asList(
+                "+E++++++++",
+                "+N++++++++",
+                "+GWALIOR++",
+                "+L++++++++",
+                "+A++++++++",
+                "+NORWAY+++",
+                "+D+++G++++",
+                "+++++R++++",
+                "+++++A++++",
+                "++++++++++"
+        );
+        assertEquals(expected, CrosswordPuzzle.crosswordPuzzle(start, words));
+    }
+
+    @Test
+    void crosswordPuzzle_4()
+    {
+        List<String> start = Arrays.asList(
+                "++++++-+++",
+                "++------++",
+                "++++++-+++",
+                "++++++-+++",
+                "+++------+",
+                "++++++-+-+",
+                "++++++-+-+",
+                "++++++++-+",
+                "++++++++-+",
+                "++++++++-+"
+        );
+        String words = "ICELAND;MEXICO;PANAMA;ALMATY";
+        List<String> expected = Arrays.asList(
+                "++++++I+++",
+                "++MEXICO++",
+                "++++++E+++",
+                "++++++L+++",
+                "+++PANAMA+",
+                "++++++N+L+",
+                "++++++D+M+",
+                "++++++++A+",
+                "++++++++T+",
+                "++++++++Y+"
+        );
+        assertEquals(expected, CrosswordPuzzle.crosswordPuzzle(start, words));
+    }
 }
