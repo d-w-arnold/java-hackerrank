132b52fdc2afd1d822ec20fa4ab97689987e2d58
David W. Arnold <d.w.arnold.gm@gmail.com>
2021-10-05 15:27:00 +0100

    Added second attempt maxXor() solution

--- src/main/java/InterviewPreparationKit/Miscellaneous/Medium/MaximumXor.java
+++ src/main/java/InterviewPreparationKit/Miscellaneous/Medium/MaximumXor.java
@@ -1,5 +1,9 @@
 package InterviewPreparationKit.Miscellaneous.Medium;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * @author David W. Arnold
  * @version 04/10/2021
@@ -16,14 +20,62 @@ public class MaximumXor
     public static int[] maxXor(int[] arr, int[] queries)
     {
         int[] ans = new int[queries.length];
-        int max;
-        for (int i = 0; i < queries.length; i++) {
-            max = 0;
-            for (int k : arr) {
-                max = Math.max(max, queries[i] ^ k);
+        Node trie = new Node();
+        Node node;
+        List<Integer> arrQueries = new ArrayList<>();
+        for (int a : arr) {
+            arrQueries.add(a);
+        }
+        for (int q : queries) {
+            arrQueries.add(q);
+        }
+        int k = Integer.toBinaryString(Collections.max(arrQueries)).length();
+        String formatStr = "";
+        List<String> numbers = new ArrayList<>();
+        for (int a : arr) {
+            formatStr = "%" + String.format("%d", k) + "s";
+            numbers.add(String.format(formatStr, Integer.toBinaryString(a)).replace(' ', '0'));
+        }
+        for (String number : numbers) {
+            node = trie;
+            for (char c : number.toCharArray()) {
+                if (c == '0') {
+                    if (node.zero == null) node.zero = new Node();
+                    node = node.zero;
+                } else if (c == '1') {
+                    if (node.one == null) node.one = new Node();
+                    node = node.one;
+                }
             }
-            ans[i] = max;
+        }
+        int index = 0;
+        String tmp;
+        StringBuilder s;
+        for (int n : queries) {
+            node = trie;
+            s = new StringBuilder();
+            for (char c : String.format(formatStr, Integer.toBinaryString(n)).replace(' ', '0').toCharArray()) {
+                tmp = Integer.toString(Integer.parseInt(Character.toString(c)) ^ 1);
+                if (tmp.equals("0")) tmp = node.zero != null ? tmp : Character.toString(c);
+                else if (tmp.equals("1")) tmp = node.one != null ? tmp : Character.toString(c);
+                s.append(tmp);
+                if (tmp.equals("0")) node = node.zero;
+                else if (tmp.equals("1")) node = node.one;
+            }
+            ans[index] = Integer.parseInt(s.toString(), 2) ^ n;
+            index++;
         }
         return ans;
+    }
+
+    static class Node
+    {
+        Node zero, one;
+
+        public Node()
+        {
+            this.zero = null;
+            this.one = null;
+        }
     }
 }
