b9f44f8de8e2f420980f4cb9473d2e486ff2131c
David W. Arnold <d.w.arnold.gm@gmail.com>
2021-10-06 16:46:44 +0100

    Started EqualStacks.equalStacks() solution

---
+++ src/main/java/ProblemSolving/Stacks/EqualStacks.java
@@ -0,0 +1,23 @@
+package ProblemSolving.Stacks;
+
+import java.util.List;
+
+/**
+ * @author David W. Arnold
+ * @version 06/10/2021
+ */
+public class EqualStacks
+{
+    /**
+     * Equal Stacks problem: https://www.hackerrank.com/challenges/equal-stacks/problem
+     *
+     * @param h1 The first array of heights.
+     * @param h2 The second array of heights.
+     * @param h3 The third array of heights.
+     * @return The height of the stacks when they are equalized.
+     */
+    public static int equalStacks(List<Integer> h1, List<Integer> h2, List<Integer> h3)
+    {
+        return -2;
+    }
+}
---
+++ src/test/java/ProblemSolving/Stacks/EqualStacksTest.java
@@ -0,0 +1,47 @@
+package ProblemSolving.Stacks;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author David W. Arnold
+ * @version 06/10/2021
+ */
+class EqualStacksTest
+{
+
+    @BeforeEach
+    void setUp()
+    {
+    }
+
+    @AfterEach
+    void tearDown()
+    {
+    }
+
+    @Test
+    void equalStacks_1()
+    {
+        assertEquals(2, EqualStacks.equalStacks(
+                Arrays.asList(1, 2, 1, 1),
+                Arrays.asList(1, 1, 2),
+                Arrays.asList(1, 1)
+        ));
+    }
+
+    @Test
+    void equalStacks_2()
+    {
+        assertEquals(5, EqualStacks.equalStacks(
+                Arrays.asList(3, 2, 1, 1, 1),
+                Arrays.asList(4, 3, 2),
+                Arrays.asList(1, 1, 4, 1)
+        ));
+    }
+}
