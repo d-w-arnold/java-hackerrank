f1c81eb42f4ed144a1327cc72711369facedc097
David W. Arnold <d.w.arnold.gm@gmail.com>
2021-10-06 14:59:02 +0100

    Added first attempt FindMaximumIndexProduct.solve() solution

--- src/main/java/ProblemSolving/Advanced/Medium/FindMaximumIndexProduct.java
+++ src/main/java/ProblemSolving/Advanced/Medium/FindMaximumIndexProduct.java
@@ -1,6 +1,6 @@
 package ProblemSolving.Advanced.Medium;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * @author David W. Arnold
@@ -16,6 +16,25 @@ public class FindMaximumIndexProduct
      */
     public static int solve(List<Integer> arr)
     {
-        return -2;
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for (int i = 0; i < arr.size(); i++) {
+            if (map.containsKey(arr.get(i))) map.get(arr.get(i)).add(i);
+            else map.put(arr.get(i), new ArrayList<>(Collections.singletonList(i)));
+        }
+        int left, right, maxAns = 0;
+        for (int i = 1; i < arr.size() - 1; i++) {
+            left = -1;
+            right = -1;
+            for (int j = i - 1; j >= 0; j--) {
+                if (arr.get(j) > arr.get(i)) left = j;
+                if (left != -1) break;
+            }
+            for (int k = i + 1; k < arr.size(); k++) {
+                if (arr.get(k) > arr.get(i)) right = k;
+                if (right != -1) break;
+            }
+            maxAns = Math.max(maxAns, (left + 1) * (right + 1));
+        }
+        return maxAns;
     }
 }
