3b54b423026aa5c8c08e81e8cf0747260984156b
David W. Arnold <d.w.arnold.gm@gmail.com>
2021-10-06 12:42:30 +0100

    Added CycleDetection.hasCycle() solution

--- src/main/java/ProblemSolving/LinkedLists/Medium/CycleDetection.java
+++ src/main/java/ProblemSolving/LinkedLists/Medium/CycleDetection.java
@@ -14,7 +14,15 @@ public class CycleDetection
      */
     public static boolean hasCycle(SinglyLinkedListNode head)
     {
-        return false;
+        if (head == null) return false;
+        SinglyLinkedListNode slow = head;
+        SinglyLinkedListNode fast = head.next;
+        while (slow != fast) {
+            if (fast == null || fast.next == null) return false;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
     }
 
     static class SinglyLinkedListNode
