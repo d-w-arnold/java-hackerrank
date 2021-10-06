b34162775a4bba2d00b63044ffe7dbb83b193d00
David W. Arnold <d.w.arnold.gm@gmail.com>
2021-10-06 15:28:47 +0100

    Added second attempt FindMaximumIndexProduct.solve() solution

--- src/main/java/ProblemSolving/Advanced/Medium/FindMaximumIndexProduct.java
+++ src/main/java/ProblemSolving/Advanced/Medium/FindMaximumIndexProduct.java
@@ -1,6 +1,7 @@
 package ProblemSolving.Advanced.Medium;
 
-import java.util.*;
+import java.util.List;
+import java.util.Stack;
 
 /**
  * @author David W. Arnold
@@ -10,31 +11,50 @@ public class FindMaximumIndexProduct
 {
     /**
      * Find Maximum Index Product problem: https://www.hackerrank.com/challenges/find-maximum-index-product/problem
+     * TODO: Improve on 59.09/100 score, pass Test cases 0 and 11.
      *
      * @param arr An array of integers.
      * @return The maximum IndexProduct(i) among all i.
      */
     public static int solve(List<Integer> arr)
     {
-        Map<Integer, List<Integer>> map = new HashMap<>();
-        for (int i = 0; i < arr.size(); i++) {
-            if (map.containsKey(arr.get(i))) map.get(arr.get(i)).add(i);
-            else map.put(arr.get(i), new ArrayList<>(Collections.singletonList(i)));
+        int[] left = nextGreaterInLeft(arr);
+        int[] right = nextGreaterInRight(arr);
+        int ans = -1;
+        for (int i = 1; i <= arr.size(); i++) {
+            ans = Math.max(ans, left[i] * right[i]);
         }
-        int left, right, maxAns = 0;
-        for (int i = 1; i < arr.size() - 1; i++) {
-            left = -1;
-            right = -1;
-            for (int j = i - 1; j >= 0; j--) {
-                if (arr.get(j) > arr.get(i)) left = j;
-                if (left != -1) break;
-            }
-            for (int k = i + 1; k < arr.size(); k++) {
-                if (arr.get(k) > arr.get(i)) right = k;
-                if (right != -1) break;
-            }
-            maxAns = Math.max(maxAns, (left + 1) * (right + 1));
+        return ans;
+    }
+
+    private static int[] nextGreaterInLeft(final List<Integer> arr)
+    {
+        int[] left_index = new int[arr.size() + 1];
+        Stack<Integer> s = new Stack<>();
+        for (int i = arr.size() - 1; i >= 0; i--) {
+            buildIndex(arr, left_index, s, i);
         }
-        return maxAns;
+        return left_index;
+    }
+
+    private static int[] nextGreaterInRight(final List<Integer> arr)
+    {
+        int[] right_index = new int[arr.size() + 1];
+        Stack<Integer> s = new Stack<>();
+        for (int i = 0; i < arr.size(); ++i) {
+            buildIndex(arr, right_index, s, i);
+        }
+        return right_index;
+    }
+
+    private static void buildIndex(final List<Integer> a, int[] index, Stack<Integer> s, int i)
+    {
+        int r;
+        while (s.size() != 0 && a.get(i) > a.get(s.peek() - 1)) {
+            r = s.peek();
+            s.pop();
+            index[r - 1] = i + 1;
+        }
+        s.push(i + 1);
     }
 }
