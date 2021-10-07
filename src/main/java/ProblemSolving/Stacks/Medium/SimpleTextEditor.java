433a11ec6d05602d6242f3eb620de764b7943ebb
David W. Arnold <d.w.arnold.gm@gmail.com>
2021-10-07 15:37:05 +0100

    Added SimpleTextEditor.simpleTextEditor() solution

--- src/main/java/ProblemSolving/Stacks/Medium/SimpleTextEditor.java
+++ src/main/java/ProblemSolving/Stacks/Medium/SimpleTextEditor.java
@@ -1,5 +1,10 @@
 package ProblemSolving.Stacks.Medium;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
 /**
  * @author David W. Arnold
  * @version 07/10/2021
@@ -11,6 +16,33 @@ public class SimpleTextEditor
      */
     public static void simpleTextEditor()
     {
-        System.out.println("NULL");
+        String s = "";
+        String[] input;
+        Stack<String> oldS = new Stack<>();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            int n = Integer.parseInt(reader.readLine());
+            for (int i = 1; i <= n; i++) {
+                input = reader.readLine().split(" ");
+                switch (Integer.parseInt(input[0])) {
+                    case 1:
+                        oldS.add(s);
+                        s += input[1];
+                        break;
+                    case 2:
+                        oldS.add(s);
+                        s = s.substring(0, s.length() - Integer.parseInt(input[1]));
+                        break;
+                    case 3:
+                        System.out.println(s.charAt(Integer.parseInt(input[1]) - 1));
+                        break;
+                    case 4:
+                        if (!oldS.isEmpty()) s = oldS.pop();
+                        break;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
