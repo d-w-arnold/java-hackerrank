301901644a341ddf6e34e37392f7c4e9e342792d
David W. Arnold <d.w.arnold.gm@gmail.com>
2021-10-14 11:01:06 +0100

    Simplified TwoDimArrayDS.hourglassSum() solution

--- src/main/java/InterviewPreparationKit/Arrays/TwoDimArrayDS.java
+++ src/main/java/InterviewPreparationKit/Arrays/TwoDimArrayDS.java
@@ -19,79 +19,39 @@ public class TwoDimArrayDS
      */
     public static int hourglassSum(List<List<Integer>> arr)
     {
-        // Get all hourglass sums.
-        List<List<Integer>> sums = new ArrayList<>();
+        int largestSum = Integer.MIN_VALUE;
         for (int i = 0; i < (arr.get(0).size() - 2); i++) {
-            List<Integer> rowOfSums = new ArrayList<>();
             for (int j = 0; j < (arr.size() - 2); j++) {
-                rowOfSums.add(sumTwoDimArrayList(Objects.requireNonNull(getHourGlass(arr, i, j))));
-            }
-            sums.add(rowOfSums);
-        }
-        // Find the maximum hourglass sum.
-        int largestSum = 0;
-        boolean firstSum = true;
-        for (List<Integer> row : sums) {
-            for (int sum : row) {
-                if (firstSum) {
-                    largestSum = sum;
-                    firstSum = false;
-                } else if (sum > largestSum) {
-                    largestSum = sum;
-                }
+                largestSum = Math.max(largestSum, sumTwoDimArrayList(Objects.requireNonNull(getHourGlass(arr, i, j))));
             }
         }
         return largestSum;
     }
 
-    /**
-     * Convert a 2D array into a 2D (array)list.
-     *
-     * @param twoDimArray The 2d array to be converted.
-     * @return The converted (array)list.
-     */
-    public static List<List<Integer>> covertTwoDimArrayToArrayList(Integer[][] twoDimArray)
-    {
-        List<List<Integer>> twoDimArrayList = new ArrayList<>();
-        for (Integer[] ints : twoDimArray) {
-            twoDimArrayList.add(Arrays.asList(ints));
-        }
-        return twoDimArrayList;
-    }
-
     private static List<List<Integer>> getHourGlass(List<List<Integer>> twoDimArrayList, int x, int y)
     {
-        if (x >= twoDimArrayList.get(0).size() || y >= twoDimArrayList.size()) {
-            return null;
-        }
+        if (x >= twoDimArrayList.get(0).size() || y >= twoDimArrayList.size()) return null;
         List<List<Integer>> hourGlass = new ArrayList<>();
-        // Get first row: a,b,c
-        List<Integer> hourGlassFirstRow = new ArrayList<>();
+        List<Integer> tmpRow = new ArrayList<>();
         for (int i = 0; i < 3; i++) {
-            hourGlassFirstRow.add(twoDimArrayList.get(x).get(y + i));
+            tmpRow.add(twoDimArrayList.get(x).get(y + i));
         }
-        hourGlass.add(hourGlassFirstRow);
-        // Get next row: d
-        List<Integer> hourGlassSecondRow = new ArrayList<>();
-        hourGlassSecondRow.add(0);
-        hourGlassSecondRow.add(twoDimArrayList.get(x + 1).get(y + 1));
-        hourGlassSecondRow.add(0);
-        hourGlass.add(hourGlassSecondRow);
-        // Get last row: e,f,g
-        List<Integer> hourGlassThirdRow = new ArrayList<>();
+        hourGlass.add(tmpRow);
+        hourGlass.add(Arrays.asList(0, twoDimArrayList.get(x + 1).get(y + 1), 0));
+        tmpRow = new ArrayList<>();
         for (int i = 0; i < 3; i++) {
-            hourGlassThirdRow.add(twoDimArrayList.get(x + 2).get(y + i));
+            tmpRow.add(twoDimArrayList.get(x + 2).get(y + i));
         }
-        hourGlass.add(hourGlassThirdRow);
+        hourGlass.add(tmpRow);
         return hourGlass;
     }
 
     private static int sumTwoDimArrayList(List<List<Integer>> twoDimArrayList)
     {
         int total = 0;
-        for (int i = 0; i < twoDimArrayList.get(0).size(); i++) {
-            for (int j = 0; j < twoDimArrayList.size(); j++) {
-                total += twoDimArrayList.get(i).get(j);
+        for (List<Integer> integers : twoDimArrayList) {
+            for (Integer integer : integers) {
+                total += integer;
             }
         }
         return total;
--- src/test/java/InterviewPreparationKit/Arrays/TwoDimArrayDSTest.java
+++ src/test/java/InterviewPreparationKit/Arrays/TwoDimArrayDSTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -15,39 +14,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  */
 class TwoDimArrayDSTest
 {
-    private final Integer[][] TWO_DIM_ARRAY_1 = {
-            {-9, -9, -9, 1, 1, 1},
-            {0, -9, 0, 4, 3, 2},
-            {-9, -9, -9, 1, 2, 3},
-            {0, 0, 8, 6, 6, 0},
-            {0, 0, 0, -2, 0, 0},
-            {0, 0, 1, 2, 4, 0}
-    };
-    private final Integer[][] TWO_DIM_ARRAY_2 = {
-            {1, 1, 1, 0, 0, 0},
-            {0, 1, 0, 0, 0, 0},
-            {1, 1, 1, 0, 0, 0},
-            {0, 0, 2, 4, 4, 0},
-            {0, 0, 0, 2, 0, 0},
-            {0, 0, 1, 2, 4, 0}
-    };
-    private final Integer[][] TWO_DIM_ARRAY_3 = {
-            {-1, -1, 0, -9, -2, -2},
-            {-2, -1, -6, -8, -2, -5},
-            {-1, -1, -1, -2, -3, -4},
-            {-1, -9, -2, -4, -4, -5},
-            {-7, -3, -3, -2, -9, -9},
-            {-1, -3, -1, -2, -4, -5}
-    };
-    private final Integer[][] TWO_DIM_ARRAY_4 = {
-            {0, -4, -6, 0, -7, -6},
-            {-1, -2, -6, -8, -3, -1},
-            {-8, -4, -2, -8, -8, -6},
-            {-3, -1, -2, -5, -7, -4},
-            {-3, -5, -3, -6, -6, -6},
-            {-3, -6, 0, -8, -6, -7}
-    };
-
     @BeforeEach
     void setUp()
     {
@@ -59,62 +25,41 @@ class TwoDimArrayDSTest
     }
 
     @Test
-    void covertTwoDimArrayToArrayList_1()
-    {
-        List<List<Integer>> twoDimArrayList = TwoDimArrayDS.covertTwoDimArrayToArrayList(TWO_DIM_ARRAY_1);
-        for (int i = 0; i < twoDimArrayList.size(); i++) {
-            assertEquals(Arrays.asList(TWO_DIM_ARRAY_1[i]), twoDimArrayList.get(i));
-        }
-    }
-
-    @Test
-    void covertTwoDimArrayToArrayList_2()
-    {
-        List<List<Integer>> twoDimArrayList = TwoDimArrayDS.covertTwoDimArrayToArrayList(TWO_DIM_ARRAY_2);
-        for (int i = 0; i < twoDimArrayList.size(); i++) {
-            assertEquals(Arrays.asList(TWO_DIM_ARRAY_2[i]), twoDimArrayList.get(i));
-        }
-    }
-
-    @Test
-    void covertTwoDimArrayToArrayList_3()
-    {
-        List<List<Integer>> twoDimArrayList = TwoDimArrayDS.covertTwoDimArrayToArrayList(TWO_DIM_ARRAY_3);
-        for (int i = 0; i < twoDimArrayList.size(); i++) {
-            assertEquals(Arrays.asList(TWO_DIM_ARRAY_3[i]), twoDimArrayList.get(i));
-        }
-    }
-
-    @Test
-    void covertTwoDimArrayToArrayList_4()
-    {
-        List<List<Integer>> twoDimArrayList = TwoDimArrayDS.covertTwoDimArrayToArrayList(TWO_DIM_ARRAY_4);
-        for (int i = 0; i < twoDimArrayList.size(); i++) {
-            assertEquals(Arrays.asList(TWO_DIM_ARRAY_4[i]), twoDimArrayList.get(i));
-        }
-    }
-
-    @Test
     void hourglassSum_1()
     {
-        assertEquals(28, TwoDimArrayDS.hourglassSum(TwoDimArrayDS.covertTwoDimArrayToArrayList(TWO_DIM_ARRAY_1)));
+        assertEquals(28, TwoDimArrayDS.hourglassSum(Arrays.asList(
+                Arrays.asList(-9, -9, -9, 1, 1, 1),
+                Arrays.asList(0, -9, 0, 4, 3, 2),
+                Arrays.asList(-9, -9, -9, 1, 2, 3),
+                Arrays.asList(0, 0, 8, 6, 6, 0),
+                Arrays.asList(0, 0, 0, -2, 0, 0),
+                Arrays.asList(0, 0, 1, 2, 4, 0)
+        )));
     }
 
     @Test
     void hourglassSum_2()
     {
-        assertEquals(19, TwoDimArrayDS.hourglassSum(TwoDimArrayDS.covertTwoDimArrayToArrayList(TWO_DIM_ARRAY_2)));
+        assertEquals(19, TwoDimArrayDS.hourglassSum(Arrays.asList(
+                Arrays.asList(1, 1, 1, 0, 0, 0),
+                Arrays.asList(0, 1, 0, 0, 0, 0),
+                Arrays.asList(1, 1, 1, 0, 0, 0),
+                Arrays.asList(0, 0, 2, 4, 4, 0),
+                Arrays.asList(0, 0, 0, 2, 0, 0),
+                Arrays.asList(0, 0, 1, 2, 4, 0)
+        )));
     }
 
     @Test
     void hourglassSum_3()
     {
-        assertEquals(-6, TwoDimArrayDS.hourglassSum(TwoDimArrayDS.covertTwoDimArrayToArrayList(TWO_DIM_ARRAY_3)));
-    }
-
-    @Test
-    void hourglassSum_4()
-    {
-        assertEquals(-6, TwoDimArrayDS.hourglassSum(TwoDimArrayDS.covertTwoDimArrayToArrayList(TWO_DIM_ARRAY_3)));
+        assertEquals(-6, TwoDimArrayDS.hourglassSum(Arrays.asList(
+                Arrays.asList(-1, -1, 0, -9, -2, -2),
+                Arrays.asList(-2, -1, -6, -8, -2, -5),
+                Arrays.asList(-1, -1, -1, -2, -3, -4),
+                Arrays.asList(-1, -9, -2, -4, -4, -5),
+                Arrays.asList(-7, -3, -3, -2, -9, -9),
+                Arrays.asList(-1, -3, -1, -2, -4, -5)
+        )));
     }
 }
