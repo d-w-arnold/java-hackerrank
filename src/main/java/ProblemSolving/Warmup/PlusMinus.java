044c8c090d476c5f0403774698507cf94dcca02c
David W. Arnold <d.w.arnold.gm@gmail.com>
2021-10-15 12:49:57 +0100

    Simplified PlusMinus.plusMinus() solution

--- src/main/java/ProblemSolving/Warmup/PlusMinus.java
+++ src/main/java/ProblemSolving/Warmup/PlusMinus.java
@@ -19,13 +19,9 @@ public class PlusMinus
         int negativeInts = 0;
         int zeros = 0;
         for (int i : arr) {
-            if (i > 0) {
-                positiveInts++;
-            } else if (i < 0) {
-                negativeInts++;
-            } else {
-                zeros++;
-            }
+            if (i > 0) positiveInts++;
+            else if (i < 0) negativeInts++;
+            else zeros++;
         }
         System.out.printf("%.6f\n", (float) positiveInts / arr.size());
         System.out.printf("%.6f\n", (float) negativeInts / arr.size());
