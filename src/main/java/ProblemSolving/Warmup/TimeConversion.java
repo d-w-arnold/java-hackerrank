661e8ecff7a00560e721d27155acadc870ff8c1c
David W. Arnold <d.w.arnold.gm@gmail.com>
2021-10-20 12:17:13 +0100

    Simplified TimeConversion.timeConversion() solution

--- src/main/java/ProblemSolving/Warmup/TimeConversion.java
+++ src/main/java/ProblemSolving/Warmup/TimeConversion.java
@@ -6,24 +6,24 @@ package ProblemSolving.Warmup;
  */
 public class TimeConversion
 {
+    /**
+     * Time Conversion problem: https://www.hackerrank.com/challenges/one-month-preparation-kit-time-conversion/problem
+     *
+     * @param s A time in 12-hour format.
+     * @return The time in 24-hour format.
+     */
     public static String timeConversion(String s)
     {
         String timeSep = ":";
         String amOrPm = s.substring(s.length() - 2);
         String time = s.substring(0, s.length() - 2);
-        String[] hoursMinsSecs = time.split(timeSep);
-        if (hoursMinsSecs[0].equals("12")) {
-            if (amOrPm.equals("PM")) {
-                return time;
-            } else if (amOrPm.equals("AM")) {
-                hoursMinsSecs[0] = "00";
-            }
-        } else if (amOrPm.equals("PM") && 1 <= Integer.parseInt(hoursMinsSecs[0]) && Integer.parseInt(hoursMinsSecs[0]) <= 11) {
-            hoursMinsSecs[0] = Integer.toString(Integer.parseInt(hoursMinsSecs[0]) + 12);
-        } else {
-            return time;
-        }
-        time = String.join(timeSep, hoursMinsSecs);
-        return time;
+        String[] hourMinSec = time.split(timeSep);
+        if (hourMinSec[0].equals("12")) {
+            if (amOrPm.equals("PM")) return time;
+            else if (amOrPm.equals("AM")) hourMinSec[0] = "00";
+        } else if (amOrPm.equals("PM") && 1 <= Integer.parseInt(hourMinSec[0]) && Integer.parseInt(hourMinSec[0]) <= 11)
+            hourMinSec[0] = Integer.toString(Integer.parseInt(hourMinSec[0]) + 12);
+        else return time;
+        return String.join(timeSep, hourMinSec);
     }
 }
