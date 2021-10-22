4aeb9b7c72a0cfe9a4757a8a280e334df7e32ed3
David W. Arnold <d.w.arnold.gm@gmail.com>
2021-10-22 12:08:14 +0100

    Simplified BreakingTheRecords.breakingRecords() solution

--- src/main/java/ProblemSolving/Implementation/BreakingTheRecords.java
+++ src/main/java/ProblemSolving/Implementation/BreakingTheRecords.java
@@ -10,17 +10,15 @@ import java.util.List;
 public class BreakingTheRecords
 {
     /**
+     * Breaking the Records problem: https://www.hackerrank.com/challenges/three-month-preparation-kit-breaking-best-and-worst-records/problem
+     *
      * @param scores Points scored per game.
      * @return An array with the numbers of times she broke her records. Index 0 is for breaking most points records, and index 1 is for breaking least points records.
      */
     public static List<Integer> breakingRecords(List<Integer> scores)
     {
-        int minimum = scores.get(0);
-        int maximum = scores.get(0);
-        int minTotal = 0;
-        int maxTotal = 0;
-        for (int i = 1; i < scores.size(); i++) {
-            int score = scores.get(i);
+        int minimum = scores.get(0), maximum = scores.get(0), minTotal = 0, maxTotal = 0;
+        for (int score : scores) {
             if (score > maximum) {
                 maximum = score;
                 maxTotal++;
